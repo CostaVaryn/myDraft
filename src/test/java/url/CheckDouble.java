e781dc3711f9e9145408f866e515e7b415b6565c
Costa Varyn <d3tug@yandex.ru>
2022-09-01 14:13:53 +0300

    add CheckDouble.java

---
+++ src/test/java/url/CheckDouble.java
@@ -0,0 +1,27 @@
+package url;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class CheckDouble {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://javarush.ru");
+        URLConnection connection = url.openConnection();
+        connection.setDoOutput(true);
+// отправляем данные
+        try (OutputStream output = connection.getOutputStream();
+             PrintStream sender = new PrintStream(output))
+        {
+            sender.println("Привет");
+        }
+
+// читаем данные
+        try(InputStream input = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input)))
+        {
+            while (reader.ready())
+                System.out.println(reader.readLine());
+        }
+    }
+}
