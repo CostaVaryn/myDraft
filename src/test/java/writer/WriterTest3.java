3cb2990dc72668a251bd3618d91648fbcb802f02
Costa Varyn <d3tug@yandex.ru>
2022-09-01 14:13:18 +0300

    add WriterTest3.java

---
+++ src/test/java/writer/WriterTest3.java
@@ -0,0 +1,24 @@
+package writer;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class WriterTest3 {
+
+    public static void main(String[] args) throws IOException {
+
+        String src = "C:\\Users\\cosva\\Desktop\\test\\log.txt";
+        OutputStream newFile = Files.newOutputStream(Paths.get(src));
+        String dest = "C:\\Users\\cosva\\Desktop\\test\\copy.txt";
+
+        try(FileReader reader = new FileReader(src);
+            FileWriter writer = new FileWriter(dest)) {
+                char[] buffer = new char[65536]; // 64Kb
+                while (reader.ready()) {
+                    int real = reader.read(buffer);
+                    writer.write(buffer, 0, real);
+            }
+        }
+    }
+}
