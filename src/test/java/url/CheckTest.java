24d8decd17614184e35790ad3bd008a3225182f0
Costa Varyn <d3tug@yandex.ru>
2022-09-01 14:14:04 +0300

    add CheckTest.java

---
+++ src/test/java/url/CheckTest.java
@@ -0,0 +1,19 @@
+package url;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CheckTest {
+
+    public static void main(String[] args) throws IOException {
+        String image = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+        URL url = new URL(image);
+        InputStream input = url.openStream();
+
+        Path path = Paths.get("C:\\GoogleLogo.png");
+        Files.copy(input, path);
+    }
+}
