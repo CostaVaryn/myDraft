98a58515e8025b855c669616925d1e1cef206852
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:46:19 +0300

    add NumbGame.java

---
+++ src/test/java/stepik/introductory/NumbGame.java
@@ -0,0 +1,19 @@
+package stepik.introductory;
+
+public class NumbGame {
+    public static void main(String[] args) {
+        int x = 1, y = 5, z = 0;
+        --y;
+        x = 4 + y++;
+        z += x--;
+        x = y + z;
+        System.out.print(++x);
+
+        int xx = 25; int yy = -12;
+        xx += yy;
+        System.out.print(xx);
+    }
+}
+
+// result:
+// x = 14;
