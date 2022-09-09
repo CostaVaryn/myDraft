22854fa4b067f444af479806c8200b014e3ba5fe
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:47:20 +0300

    add ScanEquationRoots.java

---
+++ src/test/java/stepik/introductory/scanner/ScanEquationRoots.java
@@ -0,0 +1,22 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class ScanEquationRoots {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+        sc.close();
+        double sum = (-1*b)/a;
+        double mult = c/a;
+        System.out.println(sum + " " +mult);
+    }
+}
+
+/**
+ * Sample Input:
+ * 1 2 -4
+ *
+ * Sample Output:
+ * -2.0 -4.0
+ */
