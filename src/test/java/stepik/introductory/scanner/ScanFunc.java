22eddcece5e2f489698f2dc3325ee55f00867e47
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:47:28 +0300

    add ScanFunc.java

---
+++ src/test/java/stepik/introductory/scanner/ScanFunc.java
@@ -0,0 +1,20 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class ScanFunc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        sc.close();
+        double y = 5 * (x*x) + 2*x + 11;
+        System.out.println(y);
+
+
+        /**
+         *         Scanner sc = new Scanner(System.in);
+         *         int a = sc.nextInt();
+         *         System.out.print(a + " " + a*a + " " + a*a*a);
+         */
+    }
+}
