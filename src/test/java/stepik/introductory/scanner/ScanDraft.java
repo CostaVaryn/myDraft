729611a2e018cbd9cd15873d692cb8a836d52cef
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:46:57 +0300

    add ScanDraft.java

---
+++ src/test/java/stepik/introductory/scanner/ScanDraft.java
@@ -0,0 +1,21 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class ScanDraft {
+
+    /**
+     * Sample Input:
+     * 365
+     * Sample Output:
+     * 5
+     */
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int value = sc.nextInt();
+            int myVar = value % 10; // результат 5, т.к. остаток берется целочисленный от 36,5 остаток целое число 5
+            System.out.print(myVar);
+    }
+}
+
