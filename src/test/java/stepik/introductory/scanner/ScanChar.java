ac0eac1cdf23836b1b77263881017d9208f66e3b
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:46:28 +0300

    add ScanChar.java

---
+++ src/test/java/stepik/introductory/scanner/ScanChar.java
@@ -0,0 +1,18 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class ScanChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next(), str2 = sc.next();
+        System.out.println(str1.charAt(0) < str2.charAt(0));
+
+        int x = 5, y = 3;
+        int z = ++x;
+        y += x--;
+        z+= --x + y++;
+        System.out.println(x + y + z);
+    }
+}
