69c3546bf123cda2002744b6349111fabe5c9684
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:48:15 +0300

    add ScannerTextDate.java

---
+++ src/test/java/stepik/introductory/scanner/ScannerTextDate.java
@@ -0,0 +1,13 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class ScannerTextDate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int yy = sc.nextInt();
+        String mm = sc.next();
+        int dd = sc.nextInt();
+        System.out.println(dd+":"+mm+":"+yy);
+    }
+}
