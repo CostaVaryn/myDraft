0ab74da726dd6ad8a0cb531b781920a418e79439
Costa Varyn <d3tug@yandex.ru>
2022-09-09 13:46:36 +0300

    add ScanAverage.java

---
+++ src/test/java/stepik/introductory/scanner/ScanAverage.java
@@ -0,0 +1,11 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class ScanAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble(), y = sc.nextDouble(), z = sc.nextDouble();
+        System.out.println((x+y+z)/3);
+    }
+}
