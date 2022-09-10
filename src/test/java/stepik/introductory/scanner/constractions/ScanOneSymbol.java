f8c57dde54f156b98cc682f78a97093607fe85a2
Costa Varyn <d3tug@yandex.ru>
2022-09-10 13:40:59 +0300

    add ScanOneSymbol.java

---
+++ src/test/java/stepik/introductory/scanner/constractions/ScanOneSymbol.java
@@ -0,0 +1,28 @@
+package stepik.introductory.scanner.constractions;
+
+import java.util.Scanner;
+
+public class ScanOneSymbol {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(), n = sc.next();
+        int val = str.length();
+        if (str.contains(n) || val > 20) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
+/**
+ * На вход подаётся строка, а затем - одна буква.
+ * Если строка содержит эту букву или строка длиннее 20 символов, выведите "YES", в противном случае - "NO".
+ *
+ * Sample Input:
+ * This is a string
+ * a
+ *
+ * Sample Output:
+ * YES
+ */
