ecb1c862ef712e3d88e212b292609c9c871733d4
Costa Varyn <d3tug@yandex.ru>
2022-09-10 13:41:26 +0300

    add ScanTwoOutOfThree.java

---
+++ src/test/java/stepik/introductory/scanner/constractions/ScanTwoOutOfThree.java
@@ -0,0 +1,19 @@
+package stepik.introductory.scanner.constractions;
+
+import java.util.Scanner;
+
+public class ScanTwoOutOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        if (a % 2 == 0 && b % 2 == 0 && c % 2 != 0) {
+            System.out.println("true");
+        } else if (a % 2 != 0 && b % 2 == 0 && c % 2 == 0) {
+            System.out.println("true");
+        } else if (a % 2 == 0 && b % 2 != 0 && c % 2 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
