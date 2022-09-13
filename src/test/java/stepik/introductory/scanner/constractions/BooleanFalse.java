90110025f8069bf0c9a646b86d2b8675159f4610
Costa Varyn <d3tug@yandex.ru>
2022-09-13 16:02:42 +0300

    add BooleanFalse.java

---
+++ src/test/java/stepik/introductory/scanner/constractions/BooleanFalse.java
@@ -0,0 +1,12 @@
+package stepik.introductory.scanner.constractions;
+
+import java.util.Scanner;
+
+public class BooleanFalse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        boolean z = a == 12 || b != 5 && c <= 10;
+        System.out.println(z);
+    }
+}
