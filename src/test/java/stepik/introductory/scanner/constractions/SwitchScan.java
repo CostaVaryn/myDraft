c55301dd04911f8b85cf4729b3dce547e480a26a
Costa Varyn <d3tug@yandex.ru>
2022-09-13 16:03:38 +0300

    add SwitchScan.java

---
+++ src/test/java/stepik/introductory/scanner/constractions/SwitchScan.java
@@ -0,0 +1,24 @@
+package stepik.introductory.scanner.constractions;
+
+import java.util.Scanner;
+
+public class SwitchScan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        switch (str) {
+            case "System.out.println()":
+                System.out.println("Это команда вывода на печать");
+                break;
+            case "if":
+                System.out.println("Это условный оператор");
+                break;
+            case "else":
+                System.out.println("Это альтернативная конструкция условного оператора");
+                break;
+            default:
+                System.out.println("Раздел в разработке");
+        }
+        sc.close();
+    }
+}
