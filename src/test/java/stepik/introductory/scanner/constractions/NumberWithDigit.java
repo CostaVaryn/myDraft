ee3f6359a1394558f1035e9166f4b775b7417c9b
Costa Varyn <d3tug@yandex.ru>
2022-09-13 16:03:05 +0300

    add NumberWithDigit.java

---
+++ src/test/java/stepik/introductory/scanner/constractions/NumberWithDigit.java
@@ -0,0 +1,21 @@
+package stepik.introductory.scanner.constractions;
+
+import java.util.Scanner;
+
+public class NumberWithDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = Math.abs(sc.nextInt()), y = Math.abs(sc.nextInt());
+        int z = (x%100)/10;
+        if (x > 99 && x < 1000) {
+            if (z == y) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+
+        } else {
+            System.out.println("error");
+        }
+    }
+}
