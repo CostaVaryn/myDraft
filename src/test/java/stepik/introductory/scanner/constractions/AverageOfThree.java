2058c6044cd42e8cf5a9827aecef4114fc784730
Costa Varyn <d3tug@yandex.ru>
2022-09-13 16:02:26 +0300

    add AverageOfThree.java

---
+++ src/test/java/stepik/introductory/scanner/constractions/AverageOfThree.java
@@ -0,0 +1,25 @@
+package stepik.introductory.scanner.constractions;
+
+import java.util.Scanner;
+
+public class AverageOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        if (a >= b && a <= c || a <= b && a >= c) {
+            System.out.println(a);
+        } else if (b >= a && b <= c || b <= a && b >= c) {
+            System.out.println(b);
+        } else {
+            System.out.println(c);
+        }
+    }
+}
+
+/**
+ * Sample Input:
+ * 5 10 11
+ *
+ * Sample Output:
+ * 10
+ */
