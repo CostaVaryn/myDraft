4e502b5de010f1a6e19d69198b48208a1cf8d1be
Costa Varyn <d3tug@yandex.ru>
2022-09-14 17:04:56 +0300

    add JustWhile.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/JustWhile.java
@@ -0,0 +1,18 @@
+package stepik.introductory.scanner.cycles;
+
+public class JustWhile {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "start";
+
+        while (x > 0) {
+            if (x < 10) {
+                s = "smaller";
+            } else {
+                s = "bigger";
+            }
+        }
+        s = "end";
+        System.out.println(s);
+    }
+}
