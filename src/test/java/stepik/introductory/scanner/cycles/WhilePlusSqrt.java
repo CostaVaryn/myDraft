c41e83281c17ca9f5befcc9fc3f8b19e68f15027
Costa Varyn <d3tug@yandex.ru>
2022-09-14 17:05:05 +0300

    add WhilePlusSqrt.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/WhilePlusSqrt.java
@@ -0,0 +1,25 @@
+package stepik.introductory.scanner.cycles;
+
+import java.util.Scanner;
+
+public class WhilePlusSqrt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Math.abs(sc.nextInt()), x = 1;
+        while ((x*x)<=n) {
+
+            System.out.print((x*x)+" ");
+            ++x;
+        }
+    }
+}
+
+/**
+ * На ввод подаётся натуральное число n.
+ * Выведите на печать в возрастающем порядке через пробел квадраты натуральных чисел, если эти квадраты не превышают n.
+ *
+ * Sample Input:
+ * 30
+ * Sample Output:
+ * 1 4 9 16 25
+ */
