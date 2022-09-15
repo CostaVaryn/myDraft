6f44631cadecf7596fb6ff764b20f67431dc0446
Costa Varyn <d3tug@yandex.ru>
2022-09-15 16:39:46 +0300

    add WhileAlphabetWords.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/WhileAlphabetWords.java
@@ -0,0 +1,24 @@
+package stepik.introductory.scanner.cycles;
+
+public class WhileAlphabetWords {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * На вход подаётся строка с двумя буквами, а затем - строка, состоящая из слов.
+ * Выведите на печать все слова, которые начинаются на буквы, идущие по алфавиту между буквами с первой строки (включительно).
+ * Вывести слова необходимо в том же порядке, в котором они подаются на вход.
+ *
+ * Sample Input:
+ * j p
+ * java python c++ kotlin php sql http css javascript r go
+ *
+ * Sample Output:
+ * java
+ * python
+ * kotlin
+ * php
+ * javascript
+ */
