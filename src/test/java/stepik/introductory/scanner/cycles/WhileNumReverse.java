4f77f203f48a0ebc1a618e774e3f080845885397
Costa Varyn <d3tug@yandex.ru>
2022-09-15 16:27:25 +0300

    add WhileNumReverse.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/WhileNumReverse.java
@@ -0,0 +1,22 @@
+package stepik.introductory.scanner.cycles;
+
+import java.util.Scanner;
+
+public class WhileNumReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double count = 0, sum = 0, x = 0;
+        while (sc.hasNextDouble()) {
+            double a = sc.nextDouble();
+            if (a >= 0)
+                x = a;
+            count++;
+            sum += x;
+            if (x > 10) {
+                break;
+            }
+        }
+        double res = ((sum-x)/(count-1));
+        System.out.println(res);
+    }
+}
