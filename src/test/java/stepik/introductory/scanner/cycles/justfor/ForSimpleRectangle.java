5e9a10c0076d98a14ce4c13f933ca7a1a889b2a8
Costa Varyn <d3tug@yandex.ru>
2022-09-16 16:55:39 +0300

    add ForSimpleRectangle.java

--- src/test/java/stepik/introductory/scanner/cycles/justfor/ForSimpleRectangle.java
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForSimpleRectangle.java
@@ -6,8 +6,8 @@ public class ForSimpleRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt(), y = sc.nextInt();
-        for (int i = 0; i < x; i++){
-            for (int j = 0; j < y; j++){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
                 System.out.print("*");
             }
             System.out.println();
