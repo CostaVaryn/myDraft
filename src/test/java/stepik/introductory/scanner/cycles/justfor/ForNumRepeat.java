a3c80449d07fb776229d38d9ef4ffb9ebb2c068e
Costa Varyn <d3tug@yandex.ru>
2022-09-16 16:49:46 +0300

    add ForNumRepeat.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumRepeat.java
@@ -0,0 +1,23 @@
+package stepik.introductory.scanner.cycles.justfor;
+
+import java.util.Scanner;
+
+public class ForNumRepeat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Math.abs(sc.nextInt()), b = Math.abs(sc.nextInt());
+        for (int i = 0; i < b; i++) {
+            System.out.print(a + " ");
+        }
+    }
+}
+
+/**
+ * На вход подаётся два натуральных числа - n и m.
+ * Напечатайте число n, повторённое m раз в одной строке через пробел.
+ *
+ * Sample Input:
+ * 3 5
+ * Sample Output:
+ * 3 3 3 3 3
+ */
