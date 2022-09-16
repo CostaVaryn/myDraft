0228c484d00323d111ee064f7504b54059c05fef
Costa Varyn <d3tug@yandex.ru>
2022-09-16 16:54:49 +0300

    add ForSimpleRectangle.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForSimpleRectangle.java
@@ -0,0 +1,29 @@
+package stepik.introductory.scanner.cycles.justfor;
+
+import java.util.Scanner;
+
+public class ForSimpleRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), y = sc.nextInt();
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < y; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/**
+ * На вход подаётся два натуральных числа x и y.
+ * Выведите на печать прямоугольник из звёздочек размером x*y.
+ *
+ * Sample Input:
+ * 4 2
+ * Sample Output:
+ * **
+ * **
+ * **
+ * **
+ */
