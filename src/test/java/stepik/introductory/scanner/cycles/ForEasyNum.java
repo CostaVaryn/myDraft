aa50a4c29248cd76caacfd8279f4fa06c192402a
Costa Varyn <d3tug@yandex.ru>
2022-09-16 16:15:35 +0300

    add ForEasyNum.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/ForEasyNum.java
@@ -0,0 +1,9 @@
+package stepik.introductory.scanner.cycles;
+
+public class ForEasyNum {
+    public static void main(String[] args) {
+        for (int i = 2; i < 50; i = i * i) {
+            System.out.println(i);
+        }
+    }
+}
