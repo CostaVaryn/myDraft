688fb43f01ef1992762fd6baf9f5b1784d54f9b1
Costa Varyn <d3tug@yandex.ru>
2022-09-17 13:58:45 +0300

    add ForNumSubseqDouble.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumSubseqDouble.java
@@ -0,0 +1,32 @@
+package stepik.introductory.scanner.cycles.justfor;
+
+import java.util.Scanner;
+
+public class ForNumSubseqDouble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        int count=0;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                count++;
+                if(count>n) {
+                    break;
+                }
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+/**
+ * Предположим, что мы работаем с последовательностью такого вида:
+ * 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, ... .
+ * Выведите на печать n (n > 0) членов данной последовательности в одну строку через пробел.
+ *
+ * Sample Input:
+ * 5
+ * Sample Output:
+ * 1 2 2 3 3
+ */
