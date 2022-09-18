f763b4ab75070270ef60f36a126df733c01eba14
Costa Varyn <d3tug@yandex.ru>
2022-09-18 13:17:34 +0300

    add MathConstants.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/basicmath/MathConstants.java
@@ -0,0 +1,19 @@
+package stepik.introductory.scanner.cycles.basicmath;
+
+public class MathConstants {
+    public static void main(String[] args) {
+        double x = Math.E;
+        double y = Math.PI;
+        double a = Math.pow(x, y);
+        double b = Math.pow(y, x);
+        if (a > b) {
+            System.out.println(">");
+        }
+        if (a == b) {
+            System.out.println("=");
+        }
+        if (a < b) {
+            System.out.println("<");
+        }
+    }
+}
