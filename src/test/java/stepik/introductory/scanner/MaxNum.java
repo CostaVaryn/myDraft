d207a8c262a13017df8c675cd9cfa793b4de6c51
Costa Varyn <d3tug@yandex.ru>
2022-09-18 15:17:03 +0300

    add MaxNum.java

---
+++ src/test/java/stepik/introductory/scanner/MaxNum.java
@@ -0,0 +1,20 @@
+package stepik.introductory.scanner;
+
+import java.util.Scanner;
+
+public class MaxNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Math.abs(sc.nextInt());
+
+        byte maxVal, minVal;
+        maxVal = (byte)(num % 10);
+        while (num > 0) {
+            byte curVal = (byte)(num % 10);
+            if (curVal > maxVal)
+                maxVal = curVal;
+            num /= 10;
+        }
+        System.out.println(maxVal);
+    }
+}
