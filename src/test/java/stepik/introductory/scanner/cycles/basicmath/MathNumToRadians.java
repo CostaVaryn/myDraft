719a1c5caeceafc5f87336c59cec5cac73814439
Costa Varyn <d3tug@yandex.ru>
2022-09-18 13:22:27 +0300

    add MathNumToRadians.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/basicmath/MathNumToRadians.java
@@ -0,0 +1,11 @@
+package stepik.introductory.scanner.cycles.basicmath;
+
+import java.util.Scanner;
+
+public class MathNumToRadians {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        System.out.println(Math.toRadians(x));
+    }
+}
