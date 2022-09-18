a144f26bf9415434c1b5adc09e4ac38ab9e11b7a
Costa Varyn <d3tug@yandex.ru>
2022-09-18 15:30:25 +0300

    add ForNumOddDivide.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumOddDivide.java
@@ -0,0 +1,25 @@
+package stepik.introductory.scanner.cycles.justfor;
+
+import java.util.Scanner;
+
+public class ForNumOddDivide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i<=n; i++) {
+            if (n%i==0) {
+                System.out.print(i+(i==n?"":" "));
+            }
+        }
+    }
+}
+
+/**
+ * На вход подаётся натуральное число n. В
+ * ыведите на печать все его делители в одну строку через пробел.
+ *
+ * Sample Input:
+ * 15
+ * Sample Output:
+ * 1 3 5 15
+ */
