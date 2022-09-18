86d46f5976f45db3f2c29e27e7572ea273901a63
Costa Varyn <d3tug@yandex.ru>
2022-09-18 14:27:58 +0300

    add MathPI.java

---
+++ src/test/java/stepik/introductory/scanner/cycles/basicmath/MathPI.java
@@ -0,0 +1,29 @@
+package stepik.introductory.scanner.cycles.basicmath;
+
+import java.util.Scanner;
+
+public class MathPI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (0 < a && a <= 10) {
+            double b = Math.PI;
+            double c = Math.pow(10, a);
+            System.out.println((Math.round(b * c) / c));
+
+        }
+        if (0 == a) {
+            System.out.println("3");
+        }
+    }
+}
+
+/**
+ * Дано число 0 <= n <= 10. Выведите число pi (π) с точностью n знаков после запятой.
+ * Воспользуйтесь константой PI из класса Math.
+ *
+ * Sample Input:
+ * 3
+ * Sample Output:
+ * 3.142
+ */
