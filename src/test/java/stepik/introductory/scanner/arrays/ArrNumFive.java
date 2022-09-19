4cd02c105daca5b63e2339f89156fd02af65c748
Costa Varyn <d3tug@yandex.ru>
2022-09-19 17:06:36 +0300

    add ArrNumFive.java

---
+++ src/test/java/stepik/introductory/scanner/arrays/ArrNumFive.java
@@ -0,0 +1,32 @@
+package stepik.introductory.scanner.arrays;
+
+import java.util.Scanner;
+
+public class ArrNumFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            nums[i] = nums[i] + i;
+            System.out.print(nums[i] + ((nums[i] == n) ? "" : " "));
+        }
+
+        /**
+        for (int i = 1; i <= nums.length; i++) {
+            System.out.println(i + " ");
+        }
+        */
+    }
+}
+
+/**
+ * На вход подаётся натуральное число n.
+ * Создайте массив натуральных чисел от единицы до n включительно и выведите его на печать (в одной строке, через пробел).
+ *
+ * Sample Input:
+ * 5
+ * Sample Output:
+ * 1 2 3 4 5
+ */
