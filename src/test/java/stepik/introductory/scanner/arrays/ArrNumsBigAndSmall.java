8109873bb606711fea5e1193dc485fc0f8ac0b14
Costa Varyn <d3tug@yandex.ru>
2022-09-19 18:31:24 +0300

    add ArrNumsBigAndSmall.java

---
+++ src/test/java/stepik/introductory/scanner/arrays/ArrNumsBigAndSmall.java
@@ -0,0 +1,17 @@
+package stepik.introductory.scanner.arrays;
+
+import java.util.Scanner;
+
+public class ArrNumsBigAndSmall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i <= arr.length-1; i++) {
+            arr[i] = arr[i] + sc.nextInt();
+            if (i > 0 && arr[i] >= arr[i - 1]) {
+                System.out.print(" " + arr[i]);
+            }
+        }
+    }
+}
