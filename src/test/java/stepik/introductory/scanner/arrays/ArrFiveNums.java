7764c33629ce7df7a27168cb4e65aeef27f93b30
Costa Varyn <d3tug@yandex.ru>
2022-09-19 19:04:47 +0300

    add ArrFiveNums.java

---
+++ src/test/java/stepik/introductory/scanner/arrays/ArrFiveNums.java
@@ -0,0 +1,28 @@
+package stepik.introductory.scanner.arrays;
+
+import java.util.Scanner;
+
+public class ArrFiveNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nums = sc.nextLine();
+        String[] dataHeight = nums.split(",");
+        for (String el: dataHeight) {
+            System.out.println(el);
+        }
+    }
+}
+
+/**
+ * На вход подаётся строка из элементов, разделённых запятой.
+ * Создайте из неё массив и выведите элементы массива каждый в отдельной строке.
+ *
+ * Sample Input:
+ * 1,2,3,4,5
+ * Sample Output:
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ */
