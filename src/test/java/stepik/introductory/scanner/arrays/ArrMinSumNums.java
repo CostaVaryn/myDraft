31b220187f56a141d409a4741b13cf399e3fcfce
Costa Varyn <d3tug@yandex.ru>
2022-09-21 15:46:00 +0300

    add ArrMinSumNums.java

---
+++ src/test/java/stepik/introductory/scanner/arrays/ArrMinSumNums.java
@@ -0,0 +1,34 @@
+package stepik.introductory.scanner.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrMinSumNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] nums = line.split(" ");
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] arr = new int[n];
+        for(int i = 0; i < nums.length; i++) {
+            arr[i] = Integer.valueOf(nums[i]);
+        }
+        if (nums.length <= 2) {
+            System.out.println(0);
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] + arr[2]);
+    }
+}
+
+/**
+ * Дана последовательность целых чисел.
+ * Определите минимальную сумму пар элементов массива, расстояние (разница) между индексами которых равно 2.
+ * Если таких пар нет, выведите 0.
+ *
+ * Sample Input:
+ * 8 7 6 5 4 3 2
+ * Sample Output:
+ * 6
+ */
