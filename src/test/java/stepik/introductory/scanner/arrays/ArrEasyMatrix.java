ae0b9de18c2c6a0f2cf6dc687a377f9e7012eceb
Costa Varyn <d3tug@yandex.ru>
2022-09-22 15:17:29 +0300

    add ArrEasyMatrix.java

---
+++ src/test/java/stepik/introductory/scanner/arrays/ArrEasyMatrix.java
@@ -0,0 +1,18 @@
+package stepik.introductory.scanner.arrays;
+
+import java.util.Scanner;
+
+public class ArrEasyMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        while(sc.hasNext()){
+            String[] Arr = sc.nextLine().split(" ");
+            b = Arr.length;
+            a++;
+        }
+        System.out.println("Строк: "+a);
+        System.out.print("Столбцов: "+b);
+    }
+}
