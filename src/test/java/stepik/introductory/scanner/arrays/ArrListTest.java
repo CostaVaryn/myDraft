c7a6e729e0ea5346921616887a8208a22b310afd
Costa Varyn <d3tug@yandex.ru>
2022-09-22 14:48:56 +0300

    add ArrListTest.java

---
+++ src/test/java/stepik/introductory/scanner/arrays/ArrListTest.java
@@ -0,0 +1,33 @@
+package stepik.introductory.scanner.arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrListTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+// Используем список, поскольку изначально неизвестно количество слов.
+        ArrayList<String> words = new ArrayList<>();
+
+        StringBuilder currentWord = new StringBuilder();
+        for(Character letter : s.toCharArray()){
+            // Прерываем формирование слова, если встретили пробел
+            if(letter.equals(' ')){
+                if(currentWord.length() > 0){
+                    // Записываем слово в список, если оно содержит хотя бы 1 символ
+                    words.add(currentWord.toString());
+                    currentWord = new StringBuilder();
+                }
+            } else{
+                currentWord.append(letter);
+            }
+        }
+// При завершении не забываем, что в билдере могло остаться слово.
+        if(currentWord.length() > 0){
+            words.add(currentWord.toString());
+        };
+
+    }
+}
