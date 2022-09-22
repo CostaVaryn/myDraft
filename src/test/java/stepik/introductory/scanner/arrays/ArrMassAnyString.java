c3a8f034f352964cbb2261270ffce389a6354c15
Costa Varyn <d3tug@yandex.ru>
2022-09-22 18:27:22 +0300

    update ArrMassAnyString.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrMassAnyString.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrMassAnyString.java
@@ -1,21 +1,15 @@
 package stepik.introductory.scanner.arrays;
 
-import com.sun.glass.ui.Size;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class ArrMassAnyString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-       // int size = 5;
         sc.nextLine();
-
         String[][] str = new String[size][];
-
-
         do {
             for (int i = 0; i < size; ++i) {
                 str[i] = sc.nextLine().split(" ");
@@ -23,14 +17,12 @@ public class ArrMassAnyString {
             }
         } while(sc.hasNext());
         sc.close();
-        //System.out.println(Arrays.toString(str[size]));
 
 /**
         for (int i = 0; i <= size; i++) {
                 str[i] = sc.nextLine().split(" ");
                     System.out.println(Arrays.deepToString(str[0]));
                 //System.out.println(Arrays.deepToString(str[i]));
-            //System.out.println(Arrays.toString(str[i][j]));
         }
 */
     }
