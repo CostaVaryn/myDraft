3454a9282f7fe6db2ce5e938cf1ecc74aaa30789
Costa Varyn <d3tug@yandex.ru>
2022-09-22 16:08:05 +0300

    update ArrUpendMatrix.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrUpendMatrix.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrUpendMatrix.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 public class ArrUpendMatrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int counter=0;
-        int k=0;
-        String str="";
+        int counter = 0;
+        int k = 0;
+        String str = "";
         while(sc.hasNextLine()) {
             String str1 = sc.nextLine();
-            str=str+str1+" ";
+            str = str + str1 + " ";
             counter++;
         }
-        String[] arr =str.split(" ");
+        String[] arr = str.split(" ");
         String[][] matrix = new String[counter][arr.length/counter];
-        for (int i=0 ; i<counter; i++){
-            for (int j=0 ; j<(arr.length/counter); j++) {
+        for (int i = 0 ; i < counter; i++){
+            for (int j = 0 ; j < (arr.length/counter); j++) {
                 matrix[i][j] = arr[k];
                 k++;
             }
         }
         String[][] matrix2 = new String[arr.length/counter][counter];
-        for (int x=0; x<counter ;x++) {
-            for (int y = (arr.length/counter)-1; y>=0 ;y--) {
+        for (int x = 0; x < counter ; x++) {
+            for (int y = (arr.length/counter)-1; y >= 0; y--) {
                 matrix2[y][x] = arr[k-1];
                 k--;
             }
         }
-
-        for (int m = 0; m<(arr.length/counter); m++) {
-            for (int n = 0; n<counter; n++) {
-                if (n==(counter-1)) System.out.print(matrix2[m][n]);
-                else System.out.print(matrix2[m][n] + " ");
+        for (int m = 0; m < (arr.length/counter); m++) {
+            for (int n = 0; n < counter; n++) {
+                if (n == (counter-1))
+                    System.out.print(matrix2[m][n]);
+                else
+                    System.out.print(matrix2[m][n] + " ");
             }
             System.out.println();
         }
