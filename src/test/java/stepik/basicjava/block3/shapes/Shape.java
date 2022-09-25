93748c11dea179da8f7ed2b54a223f1d00cd6599
Costa Varyn <d3tug@yandex.ru>
2022-09-25 14:24:49 +0300

    add Shape.java

---
+++ src/test/java/stepik/basicjava/block3/shapes/Shape.java
@@ -0,0 +1,14 @@
+package stepik.basicjava.block3.shapes;
+
+public class Shape {
+    private final Color color;
+    public Shape(Color color) {
+        this.color = color;
+    }
+    public Color getColor() {
+        return color;
+    }
+    public double getArea() {
+        return Double.NaN;
+    }
+}
