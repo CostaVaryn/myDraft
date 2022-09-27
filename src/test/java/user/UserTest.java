643bb7b0c9aff2f6cfcc015cc38fa647ddfa2720
Costa Varyn <d3tug@yandex.ru>
2022-09-27 15:52:32 +0300

    update UserTest.java

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -2,13 +2,10 @@ package user;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class UserTest {
     private User user;
@@ -26,12 +23,10 @@ public class UserTest {
     @Test
     public void getAllUsers() {
         List<User> expected = User.getAllUsers();
-
         List<User> actual = new ArrayList<>();
         actual.add(user);
         actual.add(user1);
         actual.add(user2);
-
         Assert.assertEquals(expected, actual);
     }
 
@@ -44,10 +39,8 @@ public class UserTest {
     @Test
     public void getAllUsers_MALE() {
         List<User> expected = User.getAllUsers(Sex.MALE);
-
         List<User> actual = new ArrayList<>();
         actual.add(user);
-
         Assert.assertEquals(expected, actual);
     }
 
@@ -61,11 +54,9 @@ public class UserTest {
     @Test
     public void getAllUsers_FEMALE() {
         List<User> expected = User.getAllUsers(Sex.FEMALE);
-
         List<User> actual = new ArrayList<>();
         actual.add(user1);
         actual.add(user2);
-
         Assert.assertEquals(expected, actual);
     }
 
@@ -79,81 +70,63 @@ public class UserTest {
     @Test
     public void getHowManyUsers() {
         int expected = User.getHowManyUsers();
-
         int actual = 3;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getHowManyUsers_MALE() {
         int expected = User.getHowManyUsers(Sex.MALE);
-
         int actual = 1;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getHowManyUsers_FEMALE() {
         int expected = User.getHowManyUsers(Sex.FEMALE);
-
         int actual = 2;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getAllAgeUsers() {
         int expected = User.getAllAgeUsers();
-
         int actual = 35 + 34 + 7;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getAverageAgeOfAllUsers() {
         int expected = User.getAverageAgeOfAllUsers();
-
         int actual = (35 + 34 + 7)/3;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getAllAgeUsers_MALE() {
         int expected = User.getAllAgeUsers(Sex.MALE);
-
         int actual = 35;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getAllAgeUsers_FEMALE() {
         int expected = User.getAllAgeUsers(Sex.FEMALE);
-
         int actual = 34 + 7;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getAverageAgeOfAllUsers_MALE() {
         int expected = User.getAverageAgeOfAllUsers(Sex.MALE);
-
         int actual = 35;
-
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getAverageAgeOfAllUsers_FEMALE() {
         int expected = User.getAverageAgeOfAllUsers(Sex.FEMALE);
-
         int actual = (34 + 7)/2;
-
         Assert.assertEquals(expected, actual);
     }
 }
