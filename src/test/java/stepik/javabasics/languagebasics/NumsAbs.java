e58c8bb59050d800ece32d8c2e112a557359c2dd
Costa Varyn <d3tug@yandex.ru>
2022-09-30 17:04:05 +0300

    add NumsAbs.java

---
+++ src/test/java/stepik/javabasics/languagebasics/NumsAbs.java
@@ -0,0 +1,24 @@
+package stepik.javabasics.languagebasics;
+
+public class NumsAbs {
+
+}
+
+/*
+Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере.
+Пробелы, знаки препинания, заглавные и строчные буквы важны!
+Вводится целое число, по модулю не превосходящее 10000.
+Выведите сначала фразу "The next number for the number ", затем введенное число, затем слово " is ", окруженное пробелами, затем формулу для следующего за введенным числа, наконец, знак точки без пробела. Аналогично в следующей строке для предыдущего числа.
+
+Sample Input 1:
+179
+Sample Output 1:
+The next number for the number 179 is 180.
+The previous number for the number 179 is 178.
+
+Sample Input 2:
+0
+Sample Output 2:
+The next number for the number 0 is 1.
+The previous number for the number 0 is -1.
+ */
