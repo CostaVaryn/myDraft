e31019df74e80e7dcdf8aec14a95a9bf45bdbd2e
Costa Varyn <d3tug@yandex.ru>
2022-10-01 16:47:46 +0300

    add NextEvenNum.java

---
+++ src/test/java/stepik/javabasics/languagebasics/NextEvenNum.java
@@ -0,0 +1,27 @@
+package stepik.javabasics.languagebasics;
+
+import java.util.Scanner;
+
+public class NextEvenNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = a-(a%2)+2;
+        System.out.println(b);
+    }
+}
+
+/*
+Дано целое число n. Выведите следующее за ним четное число.
+При решении этой задачи нельзя использовать условную инструкцию if и циклы.
+Подсказка: Подумайте, что будет оставаться в остатке при делении на 2 у чётных и у нечётных чисел.
+
+Sample Input 1:
+5
+Sample Output 1:
+6
+Sample Input 2:
+8
+Sample Output 2:
+10
+ */
