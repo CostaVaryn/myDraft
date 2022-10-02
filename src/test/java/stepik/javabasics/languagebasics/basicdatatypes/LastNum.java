cc9451a67b4bf928aafdd04633b556f0971b4dc3
Costa Varyn <d3tug@yandex.ru>
2022-10-03 00:25:38 +0300

    add LastNum.java

---
+++ src/test/java/stepik/javabasics/languagebasics/basicdatatypes/LastNum.java
@@ -0,0 +1,20 @@
+package stepik.javabasics.languagebasics.basicdatatypes;
+
+import java.util.Scanner;
+
+public class LastNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a % 10);
+    }
+}
+
+/*
+Дано произвольное положительное натуральное число. Найдите его последнюю цифру.
+
+Sample Input:
+345
+Sample Output:
+5
+ */
