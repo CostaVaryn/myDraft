9dc9b48dd8397ec1373195cea5d07112fd8bd759
Costa Varyn <d3tug@yandex.ru>
2022-10-03 17:57:42 +0300

    add Snail.java

---
+++ src/test/java/stepik/javabasics/languagebasics/basicdatatypes/Snail.java
@@ -0,0 +1,36 @@
+package stepik.javabasics.languagebasics.basicdatatypes;
+
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(), a = sc.nextInt(), b = sc.nextInt();
+        int q = h - b;
+        int p = a - b;
+        int x = q/p;
+        x = (h - 2*b + a - 1)/(a - b);
+        System.out.println(x);
+    }
+}
+
+/*
+Улитка ползёт по вертикальному шесту высотой h метров, поднимаясь за день на a метров, а за ночь спускаясь на b метров.
+На какой день улитка доползёт до вершины шеста?
+
+Программа получает на вход натуральные числа h, a, b. Гарантируется, что a>b.
+Программа должна вывести одно натуральное число.
+
+Sample Input 1:
+10
+3
+2
+Sample Output 1:
+8
+Sample Input 2:
+20
+7
+3
+Sample Output 2:
+5
+ */
