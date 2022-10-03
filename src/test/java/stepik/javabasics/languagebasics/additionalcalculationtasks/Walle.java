9a6b166ba9ca62a13a4e604e3621e011d413e16b
Costa Varyn <d3tug@yandex.ru>
2022-10-03 17:57:31 +0300

    add Walle.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/Walle.java
@@ -0,0 +1,30 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class Walle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double d = a + 3;
+        System.out.println(d/2);
+    }
+}
+
+/*
+Путешествуя, ВАЛЛ-И попал в логово к Дракону.
+Чтобы остаться в живых ему надо угадать число, которое задумали его головы.
+Первая голова называет любое целое число, вторая - увеличивает его на 3, а третья результат уменьшает в 2 раза.
+Составьте программу, которая позволит ВАЛЛ-И безошибочно вычислять результат.
+
+Входные данные:
+на вход подается натуральное число
+
+Выходные данные:
+выведите ответ на задачу
+
+Sample Input:
+10
+Sample Output:
+6.5
+ */
