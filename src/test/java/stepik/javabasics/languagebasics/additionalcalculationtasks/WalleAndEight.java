9a7203c580da8585ca7ca54149ce04ef94f5c322
Costa Varyn <d3tug@yandex.ru>
2022-10-04 14:31:57 +0300

    add WalleAndEight.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndEight.java
@@ -0,0 +1,37 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class WalleAndEight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+        System.out.println(k/n + " " + k%n);
+    }
+}
+
+/*
+ВАЛЛ-И встретил n детей и хочет разделить между ними k яблок так, чтобы каждому досталось поровну.
+Не делящийся остаток он собирается отнести ЕВЕ. Сколько яблок достанется каждому ребенку? Сколько яблок получит ЕВА?
+
+Результат выведите в одной строке через пробел в указанном порядке.
+
+Входные данные:
+два натуральных числа:
+n - количество детей
+k - количество яблок
+
+Выходные данные:
+выведите ответ на задачу
+
+Sample Input 1:
+2
+6
+Sample Output 1:
+3 0
+Sample Input 2:
+4
+25
+Sample Output 2:
+6 1
+ */
