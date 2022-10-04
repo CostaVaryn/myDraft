b1aacef52948fad9bb9fe022bcc64b55dad79990
Costa Varyn <d3tug@yandex.ru>
2022-10-04 14:32:18 +0300

    add WalleAndFifteen.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndFifteen.java
@@ -0,0 +1,30 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class WalleAndFifteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n+1);
+        System.out.print("-");
+        System.out.print(n+2);
+        System.out.print("-");
+        System.out.print(n+3);
+    }
+}
+
+/*
+ВАЛЛ-И называет натуральное число, а ЕВА записывает 3 последовательных числа, следующих после него.
+Напишите программу, которая выводит числа  ЕВЫ через черточку.
+
+Входные данные:
+натуральное число
+Выходные данные:
+выведите ответ на задачу
+
+Sample Input:
+8
+Sample Output:
+9-10-11
+ */
