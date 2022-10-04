dc7da2e58549cf21b6e0372c8e86db650c7171bb
Costa Varyn <d3tug@yandex.ru>
2022-10-04 11:04:40 +0300

    add WalleAndFour.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndFour.java
@@ -0,0 +1,41 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class WalleAndFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(); // 3
+        int t = sc.nextInt(); // 500
+        int k = sc.nextInt(); // 30
+        int n = sc.nextInt(); // 2
+        int sumTea = (t/100)*x;
+        int sumPir = x*n*k;
+        System.out.println(sumPir+sumTea);
+    }
+}
+
+/*
+ВАЛЛ-И решил отметить день рождения и пригласил x друзей.
+На каждого гостя ему необходимо приготовить 10 грамм заварки и по n пирожному.
+Килограмм чая на развес стоит t рублей, а одно пирожное - k рублей.
+Во сколько обойдется ВАЛЛ-И чаепитие? Гарантируется, что все числа в решения задачи окажутся целыми.
+
+Входные данные:
+Вводятся 4 целых неотрицательных числа:
+
+x - количество приглашенных друзей
+t - стоимость килограмма чая
+k - стоимость одного пирожного
+n - количество пирожных
+Выходные данные:
+выведите одно целое число - ответ на задачу.
+
+Sample Input:
+3
+500
+30
+2
+Sample Output:
+195
+ */
