0132167de69581dec438e5e76281ddf615392f9c
Costa Varyn <d3tug@yandex.ru>
2022-10-04 15:06:42 +0300

    add WhoIsBigger.java

---
+++ src/test/java/stepik/javabasics/languagebasics/conditionaloperator/WhoIsBigger.java
@@ -0,0 +1,28 @@
+package stepik.javabasics.languagebasics.conditionaloperator;
+
+import java.util.Scanner;
+
+public class WhoIsBigger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            if (a < b) {
+                System.out.println(b);
+            } else {
+                System.out.println(Math.max(a, b));
+            }
+        }
+    }
+}
+
+/*
+С клавиатуры вводятся два числа. Выведите на экран большее из них.
+
+Sample Input:
+8 11
+Sample Output:
+11
+ */
