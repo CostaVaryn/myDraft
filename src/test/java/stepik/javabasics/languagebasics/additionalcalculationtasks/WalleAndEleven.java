e179343e92c724f233c446b5be139738b5f95bb5
Costa Varyn <d3tug@yandex.ru>
2022-10-04 14:32:05 +0300

    add WalleAndEleven.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndEleven.java
@@ -0,0 +1,34 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class WalleAndEleven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt(), v = sc.nextInt();
+        System.out.println((double)s/v);
+    }
+}
+
+/*
+ВАЛЛ-И собирается съездить на уборку в соседний город и хочет рассчитать время на дорогу.
+По карте он определил расстояние s в километрах и настроил свою скорость равной v км/ч.
+Составьте программу, которая поможет ВАЛЛ-И определять время, проводимое в пути.
+
+Входные данные:
+s - расстояние
+v - скорость
+Выходные данные:
+выведите ответ на задачу
+
+Sample Input 1:
+600
+60
+Sample Output 1:
+10.0
+Sample Input 2:
+100
+20
+Sample Output 2:
+5.0
+ */
