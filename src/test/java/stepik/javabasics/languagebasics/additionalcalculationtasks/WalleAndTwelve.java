d3932d9e0a1652ee887c7071f110283c0d2a8e64
Costa Varyn <d3tug@yandex.ru>
2022-10-04 14:31:49 +0300

    add WalleAndTwelve.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndTwelve.java
@@ -0,0 +1,33 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class WalleAndTwelve {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), d = sc.nextInt();
+        System.out.println(n*d);
+    }
+}
+
+/*
+ВАЛЛ-И за 1 день выучивает n иностранных слов. Составьте программу, которая рассчитает,
+сколько иностранных слов выучит робот за d дней.
+
+Входные данные:
+n - количество слов
+d - количество дней
+Выходные данные:
+выведите ответ на задачу
+
+Sample Input 1:
+8
+3
+Sample Output 1:
+24
+Sample Input 2:
+10
+10
+Sample Output 2:
+100
+ */
