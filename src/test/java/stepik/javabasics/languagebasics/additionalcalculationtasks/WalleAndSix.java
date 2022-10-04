c862372917914e9b80c15113e9fb5136492af081
Costa Varyn <d3tug@yandex.ru>
2022-10-04 11:04:54 +0300

    add WalleAndSix.java

---
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndSix.java
@@ -0,0 +1,36 @@
+package stepik.javabasics.languagebasics.additionalcalculationtasks;
+
+import java.util.Scanner;
+
+public class WalleAndSix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), s = sc.nextInt();
+
+    }
+}
+
+/*
+ВАЛЛ-И очень любит читать. В его книге n страниц и каждый день он читает по s страниц.
+Составьте программу, которая считает, сколько страниц останется ВАЛЛ-И прочитать через 5 дней.
+
+Входные данные:
+Вводятся два целых неотрицательных числа:
+
+n - количество страниц в книге
+s - количество страниц, которые прочитывает ВАЛЛ-И каждый день
+Выходные данные:
+выведите на экран ответ на задачу: сколько страниц останется прочитать ВАЛЛ-И через 5 дней.
+
+Sample Input 1:
+100
+10
+Sample Output 1:
+50
+
+Sample Input 2:
+800
+20
+Sample Output 2:
+700
+ */
