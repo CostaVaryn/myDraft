7f7ff8aa2e545b0a740a3ca78003914ac4f44a63
Costa Varyn <d3tug@yandex.ru>
2022-10-05 14:31:58 +0300

    add GreatestOfThree.java

---
+++ src/test/java/stepik/javabasics/languagebasics/conditionaloperator/GreatestOfThree.java
@@ -0,0 +1,21 @@
+package stepik.javabasics.languagebasics.conditionaloperator;
+
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int res = Math.max(a,Math.max(b,c));
+        System.out.println(res);
+    }
+}
+
+/*
+Вводится 3 целых числа, выведите большее из них
+
+Sample Input:
+1 2 3
+Sample Output:
+3
+ */
