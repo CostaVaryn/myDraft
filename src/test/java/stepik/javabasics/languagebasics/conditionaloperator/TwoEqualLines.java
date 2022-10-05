e2fdb8de8200e0344ad3af96269d96ed408d3e55
Costa Varyn <d3tug@yandex.ru>
2022-10-05 14:32:12 +0300

    add TwoEqualLines.java

---
+++ src/test/java/stepik/javabasics/languagebasics/conditionaloperator/TwoEqualLines.java
@@ -0,0 +1,36 @@
+package stepik.javabasics.languagebasics.conditionaloperator;
+
+import java.util.Scanner;
+
+public class TwoEqualLines {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = sc.nextLine();
+        if ((s1.equals(s2) && !s1.equals(s3)) || (s1.equals(s3) && !s1.equals(s2)) || (s2.equals(s3) && !s2.equals(s1))) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+/*
+С клавиатуры построчно вводятся три строки.
+Если среди них есть ровно две равные строки то вывести "Yes", в противном случае - "No".
+
+Sample Input 1:
+String
+Hello!
+String
+Sample Output 1:
+Yes
+
+Sample Input 2:
+hello
+String
+Not String
+Sample Output 2:
+No
+ */
