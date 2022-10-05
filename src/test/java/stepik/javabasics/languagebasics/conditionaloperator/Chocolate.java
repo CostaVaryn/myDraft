d6f30be2ab8e53b1f9a4f692f38bef04e84ac449
Costa Varyn <d3tug@yandex.ru>
2022-10-05 14:31:52 +0300

    add Chocolate.java

---
+++ src/test/java/stepik/javabasics/languagebasics/conditionaloperator/Chocolate.java
@@ -0,0 +1,35 @@
+package stepik.javabasics.languagebasics.conditionaloperator;
+
+import java.util.Scanner;
+
+public class Chocolate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt();
+        if( (n * m > k) && (k <= 30000) && (k % n == 0 || k % m == 0)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
+/*
+Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек,
+если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
+
+Входные данные
+Вводятся 3 числа: n, m и k; k не равно n × m. Гарантируется, что количество долек в шоколадке не превосходит 30000.
+
+Выходные данные
+Программа должна вывести слово YES, если возможно отломить указанное число долек, в противном случае вывести слово NO.
+
+Sample Input 1:
+3 2 4
+Sample Output 1:
+YES
+Sample Input 2:
+3 2 1
+Sample Output 2:
+NO
+ */
