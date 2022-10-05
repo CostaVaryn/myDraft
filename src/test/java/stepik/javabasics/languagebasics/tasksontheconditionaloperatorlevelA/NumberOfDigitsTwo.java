3aa02e29006932f71c0b73459b9c34f58c99b454
Costa Varyn <d3tug@yandex.ru>
2022-10-05 15:25:14 +0300

    add NumberOfDigitsTwo.java

---
+++ src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/NumberOfDigitsTwo.java
@@ -0,0 +1,25 @@
+package stepik.javabasics.languagebasics.tasksontheconditionaloperatorlevelA;
+
+import java.util.Scanner;
+
+public class NumberOfDigitsTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = str.length();
+        System.out.println(n);
+    }
+}
+
+/*
+Вводится целое число n (0 < n < 1000). Определите, сколько в нём цифр. Выведите ответ на экран.
+
+Sample Input 1:
+8
+Sample Output 1:
+1
+Sample Input 2:
+23
+Sample Output 2:
+2
+ */
