831fdb85da878bc81b7ea1e698460bffc6c47c95
Costa Varyn <d3tug@yandex.ru>
2022-10-06 15:26:07 +0300

    add PointOne.java

---
+++ src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/PointOne.java
@@ -0,0 +1,34 @@
+package stepik.javabasics.languagebasics.tasksontheconditionaloperatorlevelA;
+
+import java.util.Scanner;
+
+public class PointOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble(), y = sc.nextDouble();
+        if (y > (2*x)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+/*
+Вводятся два действительных числа: координаты точки на плоскости.
+Определите, принадлежит ли эта точка закрашенной области. Выведите ответ на задачу в формате "Yes"/"No".
+
+Примечание: границы не являются частью закрашенной области.
+
+Sample Input 1:
+1
+3
+Sample Output 1:
+Yes
+
+Sample Input 2:
+2.5
+-1
+Sample Output 2:
+No
+ */
