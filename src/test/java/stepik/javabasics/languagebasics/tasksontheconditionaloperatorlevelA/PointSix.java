f9caa153026377104083c66af87ca488ebba1693
Costa Varyn <d3tug@yandex.ru>
2022-10-06 16:03:06 +0300

    add PointSix.java

---
+++ src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/PointSix.java
@@ -0,0 +1,35 @@
+package stepik.javabasics.languagebasics.tasksontheconditionaloperatorlevelA;
+
+import java.util.Scanner;
+
+public class PointSix {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble(), y = sc.nextDouble();
+        if (((y < ((x*x)-3)) && ((y*y) < (5-(x*x))) && (y > ((0.5*x) - 1))) ||
+                ((y > ((x*x)-3)) && ((y*y) > (5-(x*x))) && (y < ((0.5*x) - 1)))) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+/*
+Вводятся два действительных числа: координаты точки на плоскости.
+Определите, принадлежит ли эта точка закрашенной области. Выведите ответ на задачу в формате "Yes"/"No".
+
+Примечание: границы не являются частью закрашенной области.
+
+Sample Input 1:
+1
+1
+Sample Output 1:
+No
+
+Sample Input 2:
+2
+0.2
+Sample Output 2:
+Yes
+ */
