ece91804cb4f874c7360dea8f08346fa9e209b74
Costa Varyn <d3tug@yandex.ru>
2022-10-06 15:25:59 +0300

    add EquationThree.java

--- src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/EquationThree.java
+++ src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/EquationThree.java
@@ -7,12 +7,16 @@ public class EquationThree {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
         int D = b * b - 4 * a * c;
-        if (D < 0) {
-            System.out.println("0");
-        } else if (a == 0) {
+        if (a != 0) {
+            if (D < 0) {
+                System.out.println("0");
+            } else {
+                System.out.println(D == 0 ? "1" : "2");
+            }
+        } else if (a == 0 && b == 0) {
             System.out.println("inf");
         } else {
-            System.out.println(D == 0 ? "1" : "2");
+            System.out.println(a != 0 ? "1" : "0");
         }
     }
 }
@@ -22,19 +26,16 @@ public class EquationThree {
 Вам достаточно вывести одно целое число - количество корней уравнения. Если корней бесконечно много, ты выведите строку "inf".
 
 Sample Input 1:
-
 1
 -5
 6
 Sample Output 1:
-
 2
-Sample Input 2:
 
+Sample Input 2:
 1
 2
 1
 Sample Output 2:
-
 1
  */
