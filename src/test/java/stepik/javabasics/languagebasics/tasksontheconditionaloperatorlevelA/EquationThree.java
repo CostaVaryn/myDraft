1f6480edb744f5aebe49f207692b2076d585f5dd
Costa Varyn <d3tug@yandex.ru>
2022-10-06 13:57:13 +0300

    add EquationThree.java

---
+++ src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/EquationThree.java
@@ -0,0 +1,40 @@
+package stepik.javabasics.languagebasics.tasksontheconditionaloperatorlevelA;
+
+import java.util.Scanner;
+
+public class EquationThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int D = b * b - 4 * a * c;
+        if (D < 0) {
+            System.out.println("0");
+        } else if (a == 0) {
+            System.out.println("inf");
+        } else {
+            System.out.println(D == 0 ? "1" : "2");
+        }
+    }
+}
+
+/*
+Вводятся три целых числа:  a, b и c - коэффициенты квадратного уравнения. Определите количество его корней.
+Вам достаточно вывести одно целое число - количество корней уравнения. Если корней бесконечно много, ты выведите строку "inf".
+
+Sample Input 1:
+
+1
+-5
+6
+Sample Output 1:
+
+2
+Sample Input 2:
+
+1
+2
+1
+Sample Output 2:
+
+1
+ */
