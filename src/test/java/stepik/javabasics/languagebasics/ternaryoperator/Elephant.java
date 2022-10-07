4ad3c6fe981951ba238d163babe6a392dc63e1e6
Costa Varyn <d3tug@yandex.ru>
2022-10-07 13:29:21 +0300

    add Elephant.java

---
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Elephant.java
@@ -0,0 +1,36 @@
+package stepik.javabasics.languagebasics.ternaryoperator;
+
+import java.util.Scanner;
+
+public class Elephant {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double x1 = sc.nextDouble(), y1 = sc.nextDouble(), x2 = sc.nextDouble(), y2 = sc.nextDouble();
+        if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
+/*
+Требуется определить, бьет ли слон, стоящий на клетке с указанными координатами (номер строки и номер столбца),
+фигуру, стоящую на другой указанной клетке.
+
+Входные данные
+Вводятся четыре числа: координаты слон (два числа) и координаты другой фигуры (два числа),
+каждое число вводится в отдельной строке. Координаты - целые числа в интервале от 1 до 8.
+
+Выходные данные
+Требуется вывести слово YES, если слон сможет побить фигуру за 1 ход и NO - в противном случае.
+
+Sample Input 1:
+1 1 3 3
+Sample Output 1:
+YES
+Sample Input 2:
+1 1 2 3
+Sample Output 2:
+NO
+ */
