d7659faad928ddb9af46ef2a1ee8c3e73de7ed50
Costa Varyn <d3tug@yandex.ru>
2022-10-07 13:29:05 +0300

    add AreYou21.java

---
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/AreYou21.java
@@ -0,0 +1,26 @@
+package stepik.javabasics.languagebasics.ternaryoperator;
+
+import java.util.Scanner;
+
+public class AreYou21 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age > 20 ? "Да" : "Нет");
+    }
+}
+
+/*
+Вася приехал в Америку и он оооооочень любит кофе.
+В Америке приобрести этот напиток можно лишь лицам достигшим возраста 21 год.
+Напишите программу, которая считает возраст Васи и выведет ответ ("Да"/"Нет") на вопрос: попьёт ли Вася сегодня кофе?
+
+Sample Input 1:
+18
+Sample Output 1:
+Нет
+Sample Input 2:
+34
+Sample Output 2:
+Да
+ */
