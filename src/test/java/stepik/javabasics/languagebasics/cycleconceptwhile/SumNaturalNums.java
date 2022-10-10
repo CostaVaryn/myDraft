535538e4b284d070f227e6bf69b3f8ebee520ce8
Costa Varyn <d3tug@yandex.ru>
2022-10-10 13:53:13 +0300

    add SumNaturalNums.java

---
+++ src/test/java/stepik/javabasics/languagebasics/cycleconceptwhile/SumNaturalNums.java
@@ -0,0 +1,29 @@
+package stepik.javabasics.languagebasics.cycleconceptwhile;
+
+import java.util.Scanner;
+
+public class SumNaturalNums {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String nums = sc.nextLine();
+        sc.close();
+        int val = nums.length();
+        int count = 0;
+        int sum = 0;
+        String[] arr = nums.split("");
+        while (count < val) {
+            sum += Integer.valueOf(arr[count]);
+            count++;
+        }
+        System.out.println(sum);
+    }
+}
+
+/*
+Дано натуральное число N. Напишите программу, вычисляющую сумму цифр числа N.
+
+Sample Input:
+1234
+Sample Output:
+10
+ */
