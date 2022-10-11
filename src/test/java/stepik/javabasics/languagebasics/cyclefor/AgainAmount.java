f801b3a788c8ed38f85f8e526942284754ecf917
Costa Varyn <d3tug@yandex.ru>
2022-10-11 13:27:38 +0300

    add AgainAmount.java

---
+++ src/test/java/stepik/javabasics/languagebasics/cyclefor/AgainAmount.java
@@ -0,0 +1,28 @@
+package stepik.javabasics.languagebasics.cyclefor;
+
+import java.util.Scanner;
+
+public class AgainAmount {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        if (a <= b) {
+            for (int i = a; i <= b; i++) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
+/*
+Вводится 2 целых числа a и b (a <= b). Необходимо вычислить сумму всех чисел от а до b включительно.
+
+Sample Input:
+1
+3
+Sample Output:
+6
+ */
