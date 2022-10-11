5f9bc1f89c9be9afbeb712e4b70a71c1d03caa0f
Costa Varyn <d3tug@yandex.ru>
2022-10-11 13:33:44 +0300

    add GeometricProgression.java

---
+++ src/test/java/stepik/javabasics/languagebasics/cyclefor/GeometricProgression.java
@@ -0,0 +1,33 @@
+package stepik.javabasics.languagebasics.cyclefor;
+
+import java.util.Scanner;
+
+public class GeometricProgression {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += Math.pow(a,i);
+        }
+        System.out.println(sum);
+    }
+}
+
+/*
+По данному целому числу a и натуральному n вычислите сумму 1+a+a2+...+an,
+не используя формулу суммы геометрической прогрессии. Время работы программы должно быть пропорционально n.
+
+Входные данные
+Вводятся 2 числа - a и n.
+
+Выходные данные
+Необходимо вывести значение суммы.
+
+Sample Input:
+2
+2
+Sample Output:
+7
+ */
