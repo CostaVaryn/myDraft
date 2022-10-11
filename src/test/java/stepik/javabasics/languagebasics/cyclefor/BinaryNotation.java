9d09a34066fa677fdb8076c0f3cfffcce58bfba2
Costa Varyn <d3tug@yandex.ru>
2022-10-11 13:27:44 +0300

    add BinaryNotation.java

---
+++ src/test/java/stepik/javabasics/languagebasics/cyclefor/BinaryNotation.java
@@ -0,0 +1,21 @@
+package stepik.javabasics.languagebasics.cyclefor;
+
+import java.util.Scanner;
+
+public class BinaryNotation {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String str = Integer.toBinaryString(N);
+        System.out.println(new StringBuilder(str).reverse());
+    }
+}
+
+/*
+Дано натуральное число N. Выведите его представление в двоичном виде в обратном порядке.
+
+Sample Input:
+6
+Sample Output:
+011
+ */
