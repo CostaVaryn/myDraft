e2adc50280007d3f33d6ec24413a685cbe2172ba
Costa Varyn <d3tug@yandex.ru>
2022-10-11 12:21:51 +0300

    add Task9.java

---
+++ src/test/java/stepik/javabasics/languagebasics/taskswhile/Task9.java
@@ -0,0 +1,38 @@
+package stepik.javabasics.languagebasics.taskswhile;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 0;
+        while (num < n) {
+            num++;
+            for (int i = 0; i < num; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Вводятся натуральное число n. Выведите на экран лесенку, состоящую из n ступенек.
+Каждая i-ая ступенька состоит из i звёздочек.
+
+Sample Input 1:
+5
+Sample Output 1:
+*
+**
+***
+****
+*****
+Sample Input 2:
+3
+Sample Output 2:
+*
+**
+***
+ */
