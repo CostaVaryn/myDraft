3b7f8dd24cff351b440a960bae753f185f4f17c6
Costa Varyn <d3tug@yandex.ru>
2022-10-11 12:21:26 +0300

    add Task6.java

---
+++ src/test/java/stepik/javabasics/languagebasics/taskswhile/Task6.java
@@ -0,0 +1,37 @@
+package stepik.javabasics.languagebasics.taskswhile;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int count = 0;
+        while (a != 0) {
+            count += a;
+            a = sc.nextInt();
+        }
+        System.out.println(count);
+    }
+}
+
+/*
+Пользователь вводит натуральные числа по одному на каждой строке.
+Когда ему надоедает, то он вводит 0. Найти сумму введенных чисел (0 не считается)
+
+Sample Input 1:
+10
+2
+5
+5
+0
+Sample Output 1:
+22
+Sample Input 2:
+3
+2
+1
+0
+Sample Output 2:
+6
+ */
