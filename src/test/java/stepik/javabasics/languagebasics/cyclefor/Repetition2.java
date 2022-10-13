67afb5d0658e41a2733d0e6bf46425fd01bd1188
Costa Varyn <d3tug@yandex.ru>
2022-10-13 10:51:52 +0300

    add Repetition2.java

---
+++ src/test/java/stepik/javabasics/languagebasics/cyclefor/Repetition2.java
@@ -0,0 +1,32 @@
+package stepik.javabasics.languagebasics.cyclefor;
+
+import java.util.Scanner;
+
+public class Repetition2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = 0;
+        int count = 0;
+        while (s < n) {
+            int a = sc.nextInt();
+            if (a % 2 == 0) {
+                count++;
+            }
+            s++;
+        }
+        System.out.println(count);
+    }
+}
+/*
+Пользователь вводит с клавиатуры натуральное число n. Затем вводит n целых чисел.
+Необходимо найти и вывести на экран количество чётных.
+
+Sample Input:
+3
+7
+2
+9
+Sample Output:
+1
+ */
