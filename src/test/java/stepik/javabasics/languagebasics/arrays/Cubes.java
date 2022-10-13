b1e7bc51de24b0e0e1db377ee9d58f192505cf27
Costa Varyn <d3tug@yandex.ru>
2022-10-13 10:50:20 +0300

    add Cubes.java

---
+++ src/test/java/stepik/javabasics/languagebasics/arrays/Cubes.java
@@ -0,0 +1,29 @@
+package stepik.javabasics.languagebasics.arrays;
+
+import java.util.Scanner;
+
+public class Cubes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int f = sc.nextInt(), s = sc.nextInt();
+        int n = 1001;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i * i * i;
+        }
+        System.out.println(a[f]);
+        System.out.println(a[s]);
+    }
+}
+
+/*
+Создайте массив, в котором будут храниться кубы чисел от 1 до 1000.
+Затем вводятся 2 числа из этого диапазона. Используя данные из массива найдите их кубы.
+
+Sample Input:
+8
+11
+Sample Output:
+512
+1331
+ */
