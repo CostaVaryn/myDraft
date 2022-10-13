76fa4c1dbcbb8370a45aafcd9c501a9cc08cc7ce
Costa Varyn <d3tug@yandex.ru>
2022-10-13 10:49:58 +0300

    add BowlingAlley.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrListTest.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrListTest.java
@@ -8,26 +8,25 @@ public class ArrListTest {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
 
-// Используем список, поскольку изначально неизвестно количество слов.
+        // Используем список, поскольку изначально неизвестно количество слов.
         ArrayList<String> words = new ArrayList<>();
 
         StringBuilder currentWord = new StringBuilder();
-        for(Character letter : s.toCharArray()){
+        for (Character letter : s.toCharArray()) {
             // Прерываем формирование слова, если встретили пробел
-            if(letter.equals(' ')){
-                if(currentWord.length() > 0){
+            if (letter.equals(' ')) {
+                if (currentWord.length() > 0) {
                     // Записываем слово в список, если оно содержит хотя бы 1 символ
                     words.add(currentWord.toString());
                     currentWord = new StringBuilder();
                 }
-            } else{
+            } else {
                 currentWord.append(letter);
             }
         }
-// При завершении не забываем, что в билдере могло остаться слово.
-        if(currentWord.length() > 0){
+        // При завершении не забываем, что в билдере могло остаться слово.
+        if (currentWord.length() > 0) {
             words.add(currentWord.toString());
-        };
-
+        }
     }
 }
---
+++ src/test/java/stepik/javabasics/languagebasics/arrays/BowlingAlley.java
@@ -0,0 +1,50 @@
+package stepik.javabasics.languagebasics.arrays;
+
+import java.util.Scanner;
+
+public class BowlingAlley {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] pin = new String[N];
+        for (int i = 0; i < pin.length; i++) {
+            pin[i] = "I";
+        }
+        int K = sc.nextInt();
+        for (int i = 1; i <= K; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for (int j = a - 1; j < b; j++) {
+                pin[j] = ".";
+            }
+        }
+        for (String j : pin) {
+            System.out.print(j);
+        }
+    }
+}
+
+/*
+N кеглей выставили в один ряд, занумеровав их слева направо числами от 1 до N.
+Затем по этому ряду бросили K шаров, при этом i-й шар сбил все кегли с номерами от a до b включительно.
+Определите, какие кегли остались стоять на месте.
+
+Входные данные
+Программа получает на вход количество кеглей N и количество бросков K. Далее идет K пар чисел a, b , при этом 1abN.
+
+Выходные данные
+Программа должна вывести последовательность из N символов, где j-й символ есть “I”,
+если j-я кегля осталась стоять, или “.”, если j-я кегля была сбита.
+
+Sample Input:
+10
+3
+8
+10
+2
+5
+3
+6
+Sample Output:
+I.....I...
+ */
