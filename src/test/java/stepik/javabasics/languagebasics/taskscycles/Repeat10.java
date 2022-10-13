5a280db8782c7ec61f5fafcb90929b82d565d1aa
Costa Varyn <d3tug@yandex.ru>
2022-10-13 10:51:39 +0300

    add Repeat10.java

---
+++ src/test/java/stepik/javabasics/languagebasics/taskscycles/Repeat10.java
@@ -0,0 +1,30 @@
+package stepik.javabasics.languagebasics.taskscycles;
+
+import java.util.Scanner;
+
+public class Repeat10 {
+    public static void main(String[] args) {
+        String hi = "Привет";
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(hi);
+        }
+    }
+}
+
+/*
+Напишите программу, которая выводит на экран слово "Привет" (без кавычек) 10 раз.
+
+Sample Input:
+
+Sample Output:
+Привет
+Привет
+Привет
+Привет
+Привет
+Привет
+Привет
+Привет
+Привет
+Привет
+ */
