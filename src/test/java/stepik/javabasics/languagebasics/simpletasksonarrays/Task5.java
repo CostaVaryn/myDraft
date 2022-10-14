4db4709d3df8ae441b33561ddab31c36658fee2c
Costa Varyn <d3tug@yandex.ru>
2022-10-14 16:42:59 +0300

    add Task5.java

---
+++ src/test/java/stepik/javabasics/languagebasics/simpletasksonarrays/Task5.java
@@ -0,0 +1,45 @@
+package stepik.javabasics.languagebasics.simpletasksonarrays;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        if ((a >= 0 && a < n) && (b >= 0 && b < n)) {
+            System.out.println(array[a] + array[b]);
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
+
+/*
+Вводится натуральное число n. На следующей строчке вводятся n целых чисел.
+Напишите код, который создаст массив с именем array на n элементов,
+в котором буду храниться введённые числа (элемент массива равен соответствующему числу в во второй строке).
+На следующей строке вводится 2 числа - индексы элементов, сумму которых вам надо найти.
+Выведите на экран сумму этих двух элементов.
+Если вдруг индекс индекс оказался вне пределов диапазона от 0 до n - 1, то выведите на экран слово Error.
+
+Sample Input 1:
+5
+12 3 -4 6 2
+0 3
+Sample Output 1:
+18
+
+Sample Input 2:
+3
+9 -4 3
+1 10
+Sample Output 2:
+Error
+ */
