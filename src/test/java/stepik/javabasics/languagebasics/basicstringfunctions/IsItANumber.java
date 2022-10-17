c15f4cf8e257c5932d7ae63bbcb7a8849299fc48
Costa Varyn <d3tug@yandex.ru>
2022-10-17 14:24:44 +0300

    add IsItANumber.java

---
+++ src/test/java/stepik/javabasics/languagebasics/basicstringfunctions/IsItANumber.java
@@ -0,0 +1,35 @@
+package stepik.javabasics.languagebasics.basicstringfunctions;
+
+import java.util.Scanner;
+
+public class IsItANumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if (c >= '0' && c <= '9') {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
+
+/*
+Напишите программу, определяющую, является ли данный символ цифрой или нет.
+
+Входные данные
+Задан единственный символ c.
+
+Выходные данные
+Необходимо вывести  строку "yes", если символ является цифрой, и строку "no" в противном случае.
+
+Sample Input 1:
+8
+Sample Output 1:
+yes
+
+Sample Input 2:
+e
+Sample Output 2:
+no
+ */
