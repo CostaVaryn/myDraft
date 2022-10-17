5c44f35ad374ea874f23395215db64401deb67b5
Costa Varyn <d3tug@yandex.ru>
2022-10-17 18:44:13 +0300

    add CaseSensitiveCounting.java

---
+++ src/test/java/stepik/javabasics/languagebasics/stringasanarray/CaseSensitiveCounting.java
@@ -0,0 +1,25 @@
+package stepik.javabasics.languagebasics.stringasanarray;
+
+import java.util.Scanner;
+
+public class CaseSensitiveCounting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = str.replaceAll("[^f]", "")
+                .length();
+        System.out.println(count);
+    }
+}
+
+/*
+Вводится строка, найдите, сколько раз в ней встречается буква "f".
+Подсчитайте количество вхождений только "маленькой буквы".
+
+Sample Input:
+
+Good morning my dear friend. This is fish.
+Sample Output:
+
+2
+ */
