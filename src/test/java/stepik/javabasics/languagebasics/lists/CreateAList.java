4d31770455f14c6462c782c4fd990bba89583181
Costa Varyn <d3tug@yandex.ru>
2022-10-18 16:25:14 +0300

    add CreateAList.java

---
+++ src/test/java/stepik/javabasics/languagebasics/lists/CreateAList.java
@@ -0,0 +1,31 @@
+package stepik.javabasics.languagebasics.lists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CreateAList {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(0);
+        }
+        arrayList.add(1);
+        for (Integer nums : arrayList) {
+            System.out.print(nums + " ");
+        }
+    }
+}
+
+/*
+Напишите программу, которая считает натуральное число n c клавиатуры и создаст список,
+в котором первый и последний элементы будут равны строчке 1, а между ними будет n 0.
+Выведите список на экран через пробел.
+
+Sample Input:
+3
+Sample Output:
+1 0 0 0 1
+ */
