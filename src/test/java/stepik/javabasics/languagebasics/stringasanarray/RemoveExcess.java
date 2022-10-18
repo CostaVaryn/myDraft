2380cc0f4284f2144fd8d62d48449d8a812dbbbe
Costa Varyn <d3tug@yandex.ru>
2022-10-18 11:22:47 +0300

    add RemoveExcess.java

---
+++ src/test/java/stepik/javabasics/languagebasics/stringasanarray/RemoveExcess.java
@@ -0,0 +1,28 @@
+package stepik.javabasics.languagebasics.stringasanarray;
+
+import java.util.Scanner;
+
+public class RemoveExcess {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //String str = sc.nextLine().replaceAll("[^A-Za-zА-Яа-я0-9 ]", "");
+        //System.out.println(str);
+
+        char[] a = sc.nextLine().toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            if (Character.isDigit(a[i]) || Character.isLetter(a[i]) || a[i]==' ') {
+                System.out.print(a[i]);
+            }
+        }
+    }
+}
+
+/*
+Вводится строка. уберите из неё все символы, которые не являются ни цифрой, ни буквой, ни пробелом.
+
+Sample Input:
+Привет, это предложение, в котором %есть лишние ;символы. 45
+Sample Output:
+Привет это предложение в котором есть лишние символы 45
+ */
