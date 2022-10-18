7d4d844c6e7c13ca4912f7401817f1884d5969d6
Costa Varyn <d3tug@yandex.ru>
2022-10-18 16:25:09 +0300

    add AddingAnElement.java

---
+++ src/test/java/stepik/javabasics/languagebasics/lists/AddingAnElement.java
@@ -0,0 +1,37 @@
+package stepik.javabasics.languagebasics.lists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddingAnElement {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> arrayList = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(Integer.toString(i));
+        }
+        int m = sc.nextInt();
+        String str = sc.next();
+        sc.close();
+        arrayList.add(m, str);
+        for (String strs : arrayList) {
+            System.out.print(strs + " ");
+        }
+    }
+}
+
+/*
+Напишите программу, которая считает натуральное число n c клавиатуры и создаст список,
+в котором будут хранится строковые представления чисел от 0 до n-1.
+Затем программа считывает число m (m <= n) и строку s.
+Вставьте строку s на позицию m.
+Выведите список на экран через пробел.
+
+Sample Input:
+5
+2
+Строка
+Sample Output:
+0 1 Строка 2 3 4
+ */
