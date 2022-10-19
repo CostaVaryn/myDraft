ef3e32f88c60ba94b27256f8c3d1f8e72c359cd0
Costa Varyn <d3tug@yandex.ru>
2022-10-19 14:33:53 +0300

    add WhatYouSaid.java

---
+++ src/test/java/stepik/javabasics/languagebasics/setsanddictionaries/WhatYouSaid.java
@@ -0,0 +1,65 @@
+package stepik.javabasics.languagebasics.setsanddictionaries;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class WhatYouSaid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        HashMap<Integer, String> words = new HashMap<Integer, String>();
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            words.put(i, str);
+        }
+        int count = 0;
+        int max = 0;
+        String answer = "";
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (words.get(i).equals(words.get(j))) {
+                    count++;
+                }
+                if (count > max) {
+                    max = count;
+                    answer = words.get(i);
+                }
+            }
+            count = 0;
+        }
+        System.out.println(answer);
+    }
+}
+
+/*
+Вводится натуральное число n, затем n слов на разных строках.
+Определите, какое слово встречается чаще всего.
+Гарантируется, что такое слово одно.
+Все слова вводятся в нижнем регистре на русском языке.
+
+Sample Input 1:
+6
+да
+я
+прикалывался
+просто
+да
+успокойся
+Sample Output 1:
+да
+
+Sample Input 2:
+10
+просто
+будь
+собой
+просто
+будь
+весел
+просто
+будь
+добр
+будь
+Sample Output 2:
+будь
+ */
