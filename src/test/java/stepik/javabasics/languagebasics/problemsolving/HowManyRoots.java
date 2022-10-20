8b0c2108842421877875fe509963be9fa193c386
Costa Varyn <d3tug@yandex.ru>
2022-10-20 10:41:13 +0300

    add HowManyRoots.java

---
+++ src/test/java/stepik/javabasics/languagebasics/problemsolving/HowManyRoots.java
@@ -0,0 +1,24 @@
+package stepik.javabasics.languagebasics.problemsolving;
+
+import java.util.Scanner;
+
+public class HowManyRoots {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int D = (b * b) - (4 * a* c);
+        System.out.println(D == 0 ? "1" : D > 0? "2" : 0);
+    }
+}
+
+/*
+С клавиатуры вводится 3 числа: a, b, c - коэффициенты квадратного уравнения a*x2 + b*x + c = 0 (a != 0).
+Определите, сколько корней будет у этого уравнения. Выведите на экран одно число - количество корней уравнения.
+
+Sample Input:
+1
+-5
+6
+Sample Output:
+2
+ */
