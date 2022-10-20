679234ced3d1005c2a295bfa51c232c4def64289
Costa Varyn <d3tug@yandex.ru>
2022-10-20 10:33:13 +0300

    add NumberOfUniqueNumbers.java

---
+++ src/test/java/stepik/javabasics/languagebasics/setsanddictionaries/NumberOfUniqueNumbers.java
@@ -0,0 +1,29 @@
+package stepik.javabasics.languagebasics.setsanddictionaries;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class NumberOfUniqueNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int j = 0; j < n; j++) {
+            hashSet.add(arr[j]);
+        }
+    }
+}
+
+/*
+Вводится число n, затем n целых чисел. Определите, сколько различных чисел было введено.
+
+Sample Input:
+7
+1 1 2 2 2 3 3
+Sample Output:
+3
+ */
