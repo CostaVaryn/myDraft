544c5819c1f76e1a70c72e2d0e62096f0370b937
Costa Varyn <d3tug@yandex.ru>
2022-10-26 11:41:25 +0300

    update MainMats.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/writingutensils/MainMats.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/writingutensils/MainMats.java
@@ -2,6 +2,56 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.w
 
 public class MainMats {
     public static void main(String[] args) {
+
+        WritingMaterials pen = new WritingMaterials();
+        pen.setName("ручка");
+        pen.setColor("Красный");
+        pen.setLength(11.6);
+        pen.setPrice(167);
+        pen.setDraw(true);
+        pen.display();
+        pen.draw();
+        pen.priceDown(10);
+        pen.display();
+        pen.priceUp(23);
+        pen.display();
+        pen.replace_rod("Синий");
+        pen.draw();
+        System.out.println(pen.getName());
+        System.out.println(pen.getColor());
+        System.out.println(pen.getLength());
+        System.out.println(pen.getPrice());
+        System.out.println(pen.isDraw());
+
+        /*
+
+            ### lesson 2 ###
+
+        WritingMaterials pen = new WritingMaterials("ручка", "Красный", 145, 15.6, true);
+        pen.display();
+        pen.draw();
+        pen.priceDown(10);
+        pen.display();
+        pen.priceUp(23);
+        pen.display();
+        pen.replace_rod("Синий");
+        pen.draw();
+
+        WritingMaterials ruler = new WritingMaterials("линека", 34);
+        ruler.display();
+        ruler.draw();
+
+        WritingMaterials wm1 = new WritingMaterials(23, 67.8, false);
+        wm1.display();
+        wm1.draw();
+
+        WritingMaterials wm2 = new WritingMaterials("Циркуль", "Зелёный");
+        wm2.draw = true;
+        wm2.display();
+        wm2.draw();
+
+            ### lesson 1 ###
+
         WritingMaterials pen = new WritingMaterials();
         pen.name = "ручка";
         pen.color = "Красный";
@@ -26,5 +76,7 @@ public class MainMats {
         ruler.draw();
         ruler.priceUp(13);
         ruler.display();
+
+        */
     }
 }
