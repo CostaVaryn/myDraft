3a75df5cc710a5554dc3a06049b2f6a5d8f99265
Costa Varyn <d3tug@yandex.ru>
2022-10-26 11:41:19 +0300

    update WritingMaterials.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/writingutensils/WritingMaterials.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/writingutensils/WritingMaterials.java
@@ -2,6 +2,66 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.w
 
 class WritingMaterials {
 
+
+    /*
+
+        ### lesson 2 ###
+
+    String name;
+    String color;
+    double length;
+    double price;
+    boolean draw;
+
+    public WritingMaterials(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public WritingMaterials(String name, double price) {
+        this.name = name;
+        this.price = price;
+        color = "No Color";
+    }
+
+    public WritingMaterials(double length, double price, boolean draw) {
+        this.length = length;
+        this.price = price;
+        this.draw = draw;
+        name = "No Name";
+        color = "No Color";
+    }
+
+    public WritingMaterials(String name, String color, double length, double price, boolean draw) {
+        this.name = name;
+        this.color = color;
+        this.length = length;
+        this.price = price;
+        this.draw = draw;
+    }
+    public void display() {
+        System.out.printf("Название: %s, Цвет: %s, Длина: %s, ", name, color, length);
+        System.out.printf("Цена: %s, Умеет рисовать: %s.\n", price, draw ? "Да" : "Нет");
+    }
+    public void draw() {
+        if (draw) {
+            System.out.printf("%s провёл линию. Её цвет - %s.\n",name,color);
+        } else {
+            System.out.printf("%s не может ничего нарисовать.\n",name);
+        }
+    }
+    public void replace_rod(String str) {
+        this.color = str;
+    }
+    public double priceDown(double n) {
+        return price -= n;
+    }
+    public double priceUp(double n) {
+        return price += n;
+    }
+
+        ### lesson 1 ###
+
     String name;
     String color;
     double length;
@@ -30,4 +90,5 @@ class WritingMaterials {
 
     public WritingMaterials() {
     }
+    */
 }
