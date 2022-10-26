f556c8770da5e54efae00feb315999a79c597aa6
Costa Varyn <d3tug@yandex.ru>
2022-10-26 15:06:13 +0300

    update Main.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/animals/Main.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/animals/Main.java
@@ -3,6 +3,57 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.a
 public class Main {
     public static void main(String[] args) {
 
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.display();
+
+        Bird b = new Bird();
+        b.setName("Bob");
+        b.display();
+        b.setArea("На югах");
+        b.setWinterFly(false);
+        System.out.println(b.getArea());
+        System.out.println(b.isWinterFly());
+        b.chirick_chirick();
+
+        Fish f = new Fish();
+        f.setName("Сельд");
+        f.display();
+        f.setSquama("Крупная");
+        f.setUpStreamSwim(true);
+        System.out.println(f.getSquama());
+        System.out.println(f.isUpStreamSwim());
+        f.bul_bul();
+
+        Insect i = new Insect();
+        i.setName("Жук Жукыч");
+        i.display();
+        i.setWingCount(4);
+        i.setLikeJesus(true);
+        System.out.println(i.getWingCount());
+        System.out.println(i.isLikeJesus());
+        i.ggggg();
+
+        /*
+
+            ### lesson 4 ###
+
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.display();
+
+        duck.holiday();
+        duck.display();
+
+        duck.holiday(5);
+        duck.display();
+
+        duck.holiday(0.4);
+        duck.display();
+
+        duck.holiday(0.3, 2);
+        duck.display();
+
+            ### lesson 3 ###
+
         Animal tiger = new Animal("тигр", "Артём");
         tiger.setAge(15);
         tiger.setWeight(300.6);
@@ -23,9 +74,7 @@ public class Main {
         System.out.println(duck.isWalk());
         System.out.println(duck.isFly());
 
-        /*
-
-        ### lesson 2 ###
+            ### lesson 2 ###
 
         Animal tiger = new Animal("тигр", "Артём");
         tiger.age = 15;
@@ -47,7 +96,7 @@ public class Main {
         duck.rename("Утя-Утя");
         duck.display();
 
-        ### lesson 1 ###
+            ### lesson 1 ###
 
         Animal tiger = new Animal();
         tiger.type = "тигр";
