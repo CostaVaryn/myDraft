de6831bd7636b419a5511201c452810ac872c286
Costa Varyn <d3tug@yandex.ru>
2022-10-26 12:37:53 +0300

    add Salary.java

---
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/Salary.java
@@ -0,0 +1,31 @@
+package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.humans;
+
+public class Salary extends Employee {
+    private double salary; // Годовая заработная плата
+
+    public Salary(String name, String address, int number, double salary) {
+        super(name, address, number);
+        setSalary(salary);
+    }
+
+    public void mailCheck() {
+        System.out.println("Внутри mailCheck класса Salary ");
+        System.out.println("Отправляем чек " + getName()
+                + " с зарплатой " + salary);
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        if(newSalary >= 0.0) {
+            salary = newSalary;
+        }
+    }
+
+    public double computePay() {
+        System.out.println("Вычисляем заработную плату для " + getName());
+        return salary/52;
+    }
+}
