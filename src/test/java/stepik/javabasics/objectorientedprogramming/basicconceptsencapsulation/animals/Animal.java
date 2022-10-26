ebaa153bbd58dee4187ad5afac777170dd67c899
Costa Varyn <d3tug@yandex.ru>
2022-10-26 15:06:09 +0300

    update Animal.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/animals/Animal.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/animals/Animal.java
@@ -1,6 +1,77 @@
 package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.animals;
 
-class Animal {
+public class Animal {
+
+    String type;
+    String name;
+    int age;
+    double weight;
+    boolean isFly;
+    boolean isWalk;
+    boolean isSwim;
+    public Animal() {
+
+    }
+
+    public Animal(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.isFly = isFly;
+        this.isWalk = isWalk;
+        this.isSwim = isSwim;
+    }
+    public void display() {
+        System.out.printf("Тип: %s, Имя: %s, Возраст: %d, Вес: %s, ", type, name, age, weight);
+        System.out.printf("Умеет летать: %s, ", isFly ? "Да" : "Нет");
+        System.out.printf("Умеет ходить: %s, ", isWalk ? "Да" : "Нет");
+        System.out.printf("Умеет плавать: %s.\n", isSwim ? "Да" : "Нет");
+    }
+    public void setName(String str) {
+        name = str;
+    }
+
+
+
+    /*
+
+        ### lesson 4 ###
+
+    String type;
+    String name;
+    int age;
+    double weight;
+    boolean isFly;
+    boolean isWalk;
+    boolean isSwim;
+
+    public Animal(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.isFly = isFly;
+        this.isWalk = isWalk;
+        this.isSwim = isSwim;
+    }
+    public void display() {
+        System.out.printf("Тип: %s, Имя: %s, Возраст: %d, Вес: %s, ", type, name, age, weight);
+        System.out.printf("Умеет летать: %s, ", isFly ? "Да" : "Нет");
+        System.out.printf("Умеет ходить: %s, ", isWalk ? "Да" : "Нет");
+        System.out.printf("Умеет плавать: %s.\n", isSwim ? "Да" : "Нет");
+    }
+    public void holiday() {
+        weight = Math.ceil((weight+0.1)*100)/100;
+    }
+    public void holiday(double a) {
+        weight = Math.ceil((weight+a)*100)/100;
+    }
+    public void holiday(double m, int n) {
+        weight = Math.ceil((weight+(n*m))*100)/100;
+    }
+
+        ### lesson 3 ###
 
     String type;
     String name;
@@ -71,10 +142,7 @@ class Animal {
         return isFly ? "Да" : "Нет";
     }
 
-
-    /*
-
-    ### lesson 2 ###
+        ### lesson 2 ###
 
     String type;
     String name;
@@ -115,7 +183,7 @@ class Animal {
         weight = weight + 0.1 * a;
     }
 
-    ### lesson 1 ###
+        ### lesson 1 ###
 
     String type;
     String name;
@@ -137,4 +205,98 @@ class Animal {
         return weight = weight + 0.1 * a;
     }
      */
+}
+
+class Bird extends Animal {
+    String area;
+    boolean winterFly;
+    public Bird() {
+        isFly = true;
+    }
+    public Bird(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim, String area, boolean winterFly) {
+        super(type, name, age, weight, isFly, isWalk, isSwim);
+        this.area = area;
+        this.winterFly = winterFly;
+    }
+    public void chirick_chirick() {
+        System.out.println("Chirik-Chirik");
+    }
+    public void setArea(String str) {
+        area = str;
+    }
+    public void setWinterFly(boolean b) {
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public boolean isWinterFly() {
+        return winterFly;
+    }
+}
+
+class Fish extends Animal {
+    String squama;
+    boolean upStreamSwim;
+    public Fish() {
+        isSwim = true;
+    }
+
+    public Fish(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim, String squama, boolean upStreamSwim) {
+        super(type,name,age,weight,isFly,isWalk,isSwim);
+        this.squama = squama;
+        this.upStreamSwim = upStreamSwim;
+    }
+    public void bul_bul() {
+        System.out.println("Bul-bul");
+    }
+
+    public void setSquama(String squama) {
+        this.squama = squama;
+    }
+
+    public void setUpStreamSwim(boolean upStreamSwim) {
+        this.upStreamSwim = upStreamSwim;
+    }
+
+    public String getSquama() {
+        return squama;
+    }
+
+    public boolean isUpStreamSwim() {
+        return upStreamSwim;
+    }
+}
+
+class Insect extends Animal {
+    int wingCount;
+    boolean likeJesus;
+    public Insect() {
+        isWalk = true;
+    }
+    public Insect (String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim, int wingCount, boolean likeJesus) {
+        super(type,name,age,weight,isFly,isWalk,isSwim);
+        this.wingCount = wingCount;
+        this.likeJesus = likeJesus;
+    }
+    public void ggggg() {
+        System.out.println("Ggggg");
+    }
+
+    public void setWingCount(int wingCount) {
+        this.wingCount = wingCount;
+    }
+
+    public void setLikeJesus(boolean likeJesus) {
+        this.likeJesus = likeJesus;
+    }
+
+    public int getWingCount() {
+        return wingCount;
+    }
+
+    public boolean isLikeJesus() {
+        return likeJesus;
+    }
 }
