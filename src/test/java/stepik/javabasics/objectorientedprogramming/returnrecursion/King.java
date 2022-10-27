c5b62a834c9786a0ab4f3eb9605e3be21ebe55a3
Costa Varyn <d3tug@yandex.ru>
2022-10-27 17:22:46 +0300

    correction King.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/returnrecursion/King.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/returnrecursion/King.java
@@ -3,7 +3,7 @@ package stepik.javabasics.objectorientedprogramming.returnrecursion;
 import java.util.Scanner;
 
 public class King {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
@@ -11,7 +11,7 @@ public class King {
         int y2 = sc.nextInt();
         king_step(x1, y1, x2, y2);
     }
-    private static void king_step(int x1, int y1, int x2, int y2){
+    private static void king_step(int x1, int y1, int x2, int y2) {
         if (x1 > 8 || x1 < 1 || y1 > 8 || y1 < 1) {
             System.out.println("1");
         } else if (x2 > 8 || x2 < 1 || y2 > 8 || y2 < 1) {
