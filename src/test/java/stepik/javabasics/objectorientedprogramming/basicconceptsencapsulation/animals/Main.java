c4661bdaf894c178d5259e7f81dc7a093b5c6a52
Costa Varyn <d3tug@yandex.ru>
2022-10-28 15:53:31 +0300

    update Main.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/animals/Main.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/animals/Main.java
@@ -3,6 +3,36 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.a
 public class Main {
     public static void main(String[] args) {
 
+        Animal duck = new Animal();
+        duck.display();
+
+        Bird b = new Bird();
+        b.setName("Bob");
+        b.setArea("На югах");
+        b.setWinterFly(false);
+        b.display();
+        System.out.println(b.getArea());
+        System.out.println(b.isWinterFly());
+        b.chirick_chirick();
+
+        Fish f = new Fish();
+        f.setName("Сельд");
+        f.setSquama("Крупная");
+        f.setUpStreamSwim(true);
+        f.display();
+        System.out.println(f.getSquama());
+        System.out.println(f.isUpStreamSwim());
+        f.bul_bul();
+
+        Insect i = new Insect();
+        i.setName("Жук Жукыч");
+        i.setWingCount(4);
+        i.setLikeJesus(true);
+        i.display();
+        System.out.println(i.getWingCount());
+        System.out.println(i.isLikeJesus());
+        i.ggggg();
+
         /*
 
         ### lesson 5 ###
