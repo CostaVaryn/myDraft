7e091ff1348e3a28889a3bac46da0cb68962ceb0
Costa Varyn <d3tug@yandex.ru>
2022-10-28 16:33:58 +0300

    update Program.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/Program.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/Program.java
@@ -3,6 +3,15 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.h
 public class Program {
     public static void main(String[] args) {
 
+        Employee sam = new Employee("Sam", "Leman Brothers");
+        sam.display();
+        Client bob = new Client("Bob", "Leman Brothers");
+        bob.display();
+
+        /*
+
+        ### lesson 1 ###
+
         Person kate = new Person("Kate", 32, "Baker Street", "+12334567");
         kate.displayName();     // норм, метод public
         kate.displayAge();      // норм, метод имеет модификатор по умолчанию
@@ -26,5 +35,7 @@ public class Program {
 
         Person undef = new Person();
         undef.displayInfo();
+
+         */
     }
 }
