a67d948786ba2c09f5f3d8c6b21cf6b73dc73f76
Costa Varyn <d3tug@yandex.ru>
2022-10-28 16:34:18 +0300

    update Client.java

---
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/Client.java
@@ -0,0 +1,16 @@
+package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.humans;
+
+public class Client extends Person {
+    private String bank;
+
+    public Client(String name, String company) {
+
+        super(name);
+        this.bank = company;
+    }
+
+    public void display(){
+
+        System.out.printf("Client Name: %s \t Bank: %s \n", super.getName(), bank);
+    }
+}
