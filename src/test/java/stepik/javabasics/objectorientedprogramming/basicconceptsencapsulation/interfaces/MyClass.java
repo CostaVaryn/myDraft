1f4f9f1895d964a80a22cf805a4dfc5b289cb3a2
Costa Varyn <d3tug@yandex.ru>
2022-10-29 16:57:44 +0300

    add MyClass.java

---
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/interfaces/MyClass.java
@@ -0,0 +1,13 @@
+package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.interfaces;
+
+public class MyClass implements SomeClass.Callback {
+    @Override
+    public void callingBack(String s) {
+
+        if (s != null) {
+            System.out.println("Ваш ответ: " + s);
+        } else {
+            System.out.println("Вы не ответили на вопрос!");
+        }
+    }
+}
