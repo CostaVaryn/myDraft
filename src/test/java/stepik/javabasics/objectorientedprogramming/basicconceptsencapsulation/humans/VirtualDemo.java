c61dfb8f12ecd2a65df2f31a196c05be1045d8f5
Costa Varyn <d3tug@yandex.ru>
2022-10-29 16:57:03 +0300

    update VirtualDemo.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/VirtualDemo.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/VirtualDemo.java
@@ -2,11 +2,11 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.h
 
 public class VirtualDemo {
     public static void main(String [] args) {
-        Salary s = new Salary("Олег Петров", "Минск, Беларусь", 3, 3600.00);
-        Employee e = new Salary("Иван Иванов", "Москва, Россия", 2, 2400.00);
+        //Salary s = new Salary("Олег Петров", "Минск, Беларусь", 3, 3600.00);
+        //Employee e = new Salary("Иван Иванов", "Москва, Россия", 2, 2400.00);
         System.out.println("Вызываем mailCheck, используя ссылку Salary --");
-        s.mailCheck();
+        //s.mailCheck();
         System.out.println("Вызываем mailCheck, используя ссылку Employee --");
-        e.mailCheck();
+        //e.mailCheck();
     }
 }
