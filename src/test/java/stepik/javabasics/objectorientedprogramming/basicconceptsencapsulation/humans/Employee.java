40f13c00ed5fb378c3465b5e4846cce517666f17
Costa Varyn <d3tug@yandex.ru>
2022-10-29 16:56:36 +0300

    update Employee.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/Employee.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/Employee.java
@@ -3,6 +3,9 @@ package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.h
 public class Employee extends Person{
 
     private String bank;
+    private String name;
+    private String address;
+    private int number;
 
     public Employee(String name, String company) {
 
@@ -10,10 +13,35 @@ public class Employee extends Person{
         this.bank = company;
     }
 
+
     public void display(){
 
         System.out.printf("Employee Name: %s \t Bank: %s \n", super.getName(), bank);
     }
+    public void mailCheck() {
+        System.out.println("Отправляем чек " + this.name + " " + this.address);
+    }
+
+    public String toString() {
+        return name + " " + address + " " + number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
 
     /*
 
