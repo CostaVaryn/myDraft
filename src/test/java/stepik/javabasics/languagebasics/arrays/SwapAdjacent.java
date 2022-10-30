b03b276c8e7f6f17cd5a79fa4774903bd2643d72
Costa Varyn <d3tug@yandex.ru>
2022-10-30 15:02:54 +0300

    update SwapAdjacent.java

--- src/test/java/stepik/javabasics/languagebasics/arrays/SwapAdjacent.java
+++ src/test/java/stepik/javabasics/languagebasics/arrays/SwapAdjacent.java
@@ -8,7 +8,6 @@ public class SwapAdjacent {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-
         if (n > 1) {
             for (int i = 0; i < n - 1; i += 2) {
                 int temp = arr[i];
