e784a41f1eb1b4166fc1a70a7ebab7b5813e2199
Costa Varyn <d3tug@yandex.ru>
2022-10-31 12:34:17 +0300

    update MainBuilders.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/buildings/MainBuilders.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/buildings/MainBuilders.java
@@ -1,7 +1,13 @@
 package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.buildings;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class MainBuilders {
     public static void main(String[] args) {
-
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+        System.out.println("Сегодня: " + format.format(date));
     }
 }
