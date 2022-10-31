bfb35540bc081bfed28e08f67d796c9b8c239ad2
Costa Varyn <d3tug@yandex.ru>
2022-10-31 16:04:42 +0300

    add PrimeCalc.java

---
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/interfaces/PrimeCalc.java
@@ -0,0 +1,30 @@
+package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.interfaces;
+
+public class PrimeCalc {
+    public static void main(String[] args) {
+
+        Calculatable c = new Calculation();
+        System.out.println(c.sum(1, 2));
+        System.out.println(c.sum(1, 2, 4));
+    }
+}
+class Calculation implements Calculatable{
+
+}
+interface Calculatable{
+
+    default int sum(int a, int b){
+        return sumAll(a, b);
+    }
+    default int sum(int a, int b, int c){
+        return sumAll(a, b, c);
+    }
+
+    static int sumAll(int... values){
+        int result = 0;
+        for(int n : values){
+            result += n;
+        }
+        return result;
+    }
+}
