20b8f09f57b3a2431e0f36204683681dd242e2d4
Costa Varyn <d3tug@yandex.ru>
2022-10-31 12:19:15 +0300

    update Building.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/buildings/Building.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/buildings/Building.java
@@ -7,7 +7,21 @@ abstract class Building {
     String architectName;
     boolean isCulturalMonument;
 
+    public String getTitle() {
+        return title;
+    }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public int getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public String getArchitectName() {
+        return architectName;
+    }
 
     public Building(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
         this.title = title;
@@ -19,37 +33,52 @@ abstract class Building {
 }
 
 class Library extends Building {
-    public Library(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
+    boolean hasAWebsite;
+    public Library(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument, boolean hasAWebsite) {
         super(title, address, yearOfConstruction, architectName, isCulturalMonument);
+        this.hasAWebsite = hasAWebsite;
+    }
+    public void startEvent(String event) {
+        System.out.println("Сегодня: " + event);
     }
 }
 
 class House extends Building {
-    public House(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
+    boolean isPrivate;
+    public House(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument, boolean isPrivate) {
         super(title, address, yearOfConstruction, architectName, isCulturalMonument);
+        this.isPrivate = isPrivate;
     }
 }
 
 class PoliceDepartment extends Building {
-    public PoliceDepartment(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
+    boolean isFBI;
+    public PoliceDepartment(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument, boolean isFBI) {
         super(title, address, yearOfConstruction, architectName, isCulturalMonument);
+        this.isFBI = isFBI;
     }
 }
 
 class University extends Building {
-    public University(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
+    boolean isAcademy;
+    public University(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument, boolean isAcademy) {
         super(title, address, yearOfConstruction, architectName, isCulturalMonument);
+        this.isAcademy = isAcademy;
     }
 }
 
 class ShoppingCenter extends Building {
-    public ShoppingCenter(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
+    boolean isCountry;
+    public ShoppingCenter(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument, boolean isCountry) {
         super(title, address, yearOfConstruction, architectName, isCulturalMonument);
+        this.isCountry = isCountry;
     }
 }
 
 class HighRiseBuilding extends Building {
-    public HighRiseBuilding(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument) {
+    boolean isMemorial;
+    public HighRiseBuilding(String title, String address, int yearOfConstruction, String architectName, boolean isCulturalMonument, boolean isMemorial) {
         super(title, address, yearOfConstruction, architectName, isCulturalMonument);
+        this.isMemorial = isMemorial;
     }
 }
