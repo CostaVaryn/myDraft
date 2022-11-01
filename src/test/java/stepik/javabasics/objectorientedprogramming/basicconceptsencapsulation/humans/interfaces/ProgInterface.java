6672178619bb7573d1e449b6a49af4264efb86f8
Costa Varyn <d3tug@yandex.ru>
2022-11-01 12:50:56 +0300

    relocated ProgInterface.java

--- src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/interfaces/ProgInterface.java
+++ src/test/java/stepik/javabasics/objectorientedprogramming/basicconceptsencapsulation/humans/interfaces/ProgInterface.java
@@ -1,4 +1,4 @@
-package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.interfaces;
+package stepik.javabasics.objectorientedprogramming.basicconceptsencapsulation.humans.interfaces;
 
 public class ProgInterface {
 
