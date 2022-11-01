487fb3717b8734c0bc190f8bc27a62fef971d2f6
Costa Varyn <d3tug@yandex.ru>
2022-11-01 12:51:36 +0300

    added ExTryCatchFin.java

---
+++ src/test/java/stepik/javabasics/objectorientedprogramming/exceptionhandling/ExTryCatchFin.java
@@ -0,0 +1,17 @@
+package stepik.javabasics.objectorientedprogramming.exceptionhandling;
+
+public class ExTryCatchFin {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        try{
+            numbers[6]=45;
+            numbers[6]=Integer.parseInt("gfd");
+        }
+        catch(ArrayIndexOutOfBoundsException ex){
+            System.out.println("Выход за пределы массива");
+        }
+        catch(NumberFormatException ex){
+            System.out.println("Ошибка преобразования из строки в число");
+        }
+    }
+}
