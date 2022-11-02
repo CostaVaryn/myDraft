1dd5291e36df7ecca32c13660453f199b93d4cc2
Costa Varyn <d3tug@yandex.ru>
2022-11-02 13:46:31 +0300

    added ContentPaneReplace.java

---
+++ src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/ContentPaneReplace.java
@@ -0,0 +1,27 @@
+package stepik.javabasics.graphicaluserinterface.graphicsinjava;
+
+import javax.swing.*;
+public class ContentPaneReplace extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    public ContentPaneReplace() {
+        super("Test ContentPane");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Создание панели с двумя кнопками
+        JPanel contents = new JPanel();
+        contents.add(new JButton("Семья"));
+        contents.add(new JButton("Школа"));
+        // Замена панели содержимого
+        setContentPane(contents);
+
+        // Определение размера окна
+        setSize(200, 100);
+        // Открытие окна
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new ContentPaneReplace();
+    }
+}
