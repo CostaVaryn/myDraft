58a5ae8bee013bde3f8b0d076358c19823af33c6
Costa Varyn <d3tug@yandex.ru>
2022-11-02 15:11:23 +0300

    relocated MiniWin.java

--- src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/winpanel/MiniWin.java
+++ src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/winpanel/MiniWin.java
@@ -1,4 +1,4 @@
-package stepik.javabasics.graphicaluserinterface.graphicsinjava;
+package stepik.javabasics.graphicaluserinterface.graphicsinjava.winpanel;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
