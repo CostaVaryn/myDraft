0806ab02022164f7c47d56a70faaf0ccee25c2c3
Costa Varyn <d3tug@yandex.ru>
2022-11-03 11:33:35 +0300

    added FlowLayoutTest.java

---
+++ src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/layout/FlowLayoutTest.java
@@ -0,0 +1,28 @@
+package stepik.javabasics.graphicaluserinterface.graphicsinjava.layout;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class FlowLayoutTest extends JFrame {
+    public FlowLayoutTest() {
+        super("FlowLayout");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 120);
+        // Панель содержимого
+        Container container = getContentPane();
+        /*
+         * Определение последовательного расположения
+         * с выравниванием компонентов по центру
+         */
+        container.setLayout (new FlowLayout(FlowLayout.CENTER));
+        // добавляем компоненты
+        container.add(new JButton("Школа"));
+        container.add(new JButton("Институт"));
+        container.add(new JButton("Академия"));
+        // Открываем окно
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new FlowLayoutTest();
+    }
+}
