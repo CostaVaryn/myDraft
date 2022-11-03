b1965a086451110759aa7a464c39b56fd3d1ce6b
Costa Varyn <d3tug@yandex.ru>
2022-11-03 11:10:30 +0300

    updated ShapesPanel.java

--- src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/ShapesPanel.java
+++ src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/ShapesPanel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 
 class Example extends JFrame {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         GraphicsDemo myGraphicsFrame = new GraphicsDemo();
         myGraphicsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
