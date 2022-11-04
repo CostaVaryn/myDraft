a7c2b30beaf9795605bd10703c2b691fa255a364
Costa Varyn <d3tug@yandex.ru>
2022-11-04 10:42:05 +0300

    added MainMenu.java

---
+++ src/test/java/stepik/javabasics/graphicaluserinterface/graphicsinjava/box/MainMenu.java
@@ -0,0 +1,45 @@
+package stepik.javabasics.graphicaluserinterface.graphicsinjava.box;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu {
+    JFrame mainMenu;
+    JPanel panel;
+    JLabel lLog;
+    JTextField logText;
+    JLabel lPas;
+    JPasswordField passwordField;
+    JButton btnOk;
+    JButton btnCancel;
+
+    public MainMenu() {
+        JLabel lLog = new JLabel("LOGIN: ");
+        JTextField logText = new JTextField(13);
+        JLabel lPas = new JLabel("PASS: ");
+        JPasswordField passwordField = new JPasswordField(10);
+        JButton btnOk = new JButton("Ok");
+        JButton btnCancel = new JButton("Cancel");
+        Box one = Box.createHorizontalBox();
+        one.add(lLog);
+        one.add(logText);
+        Box two = Box.createHorizontalBox();
+        two.add(lPas);
+        two.add(Box.createRigidArea(new Dimension(10, 0)));
+        two.add(passwordField);
+        Box three = Box.createHorizontalBox();
+        three.add(Box.createRigidArea(new Dimension(45, 0)));
+        btnOk.setAlignmentX(Component.LEFT_ALIGNMENT);
+        three.add(btnOk);
+        btnCancel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        three.add(btnCancel);
+        Box vert = Box.createVerticalBox();
+        vert.add(Box.createRigidArea(new Dimension(0, 200)));
+        vert.add(one);
+        vert.add(two);
+        vert.add(three);
+        //JPanel panel = new JPanel(vert.getLayout());
+        //JPanel panel = new JPanel();
+        //JFrame mainMenu = new JFrame("Main Menu", 490, 550, panel);
+    }
+}
