e87246fca77c26640193f87dd38d67ea740bc6c8
Costa Varyn <d3tug@yandex.ru>
2022-11-09 13:16:55 +0300

    added SipmleSwitch.java

---
+++ src/test/java/stepik/javabasics/languagebasics/SipmleSwitch.java
@@ -0,0 +1,27 @@
+package stepik.javabasics.languagebasics;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class SipmleSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int value;
+        switch (count) {
+            case 1:
+                value = 12;
+                break;
+            case 2:
+                value = 32;
+                break;
+            case 3:
+                value = 52;
+                break;
+            default:
+                value = 0;
+        }
+        System.out.println(value);
+    }
+}
