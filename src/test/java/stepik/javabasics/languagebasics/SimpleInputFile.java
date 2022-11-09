fa9147f1111c17738106988bf4c74f757edacaa3
Costa Varyn <d3tug@yandex.ru>
2022-11-09 13:16:37 +0300

    added SimpleInputFile.java

---
+++ src/test/java/stepik/javabasics/languagebasics/SimpleInputFile.java
@@ -0,0 +1,26 @@
+package stepik.javabasics.languagebasics;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SimpleInputFile {
+    public static void main(String[] args) {
+        String src = "notes1.txt";
+        String dest = "notes3.txt";
+
+        try(FileInputStream input = new FileInputStream(src);
+            FileOutputStream output = new FileOutputStream(dest)) {
+            byte[] buffer = new byte[65536]; // 64Kb
+            while (input.available() > 0) {
+                int real = input.read(buffer);
+                output.write(buffer, 0, real);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
