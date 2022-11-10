937c6659e83fb5fe5ba26b07176128b51e96c59b
Costa Varyn <d3tug@yandex.ru>
2022-11-10 11:48:05 +0300

    added TestMaven.java

---
+++ src/test/java/stepik/javacore/libraryjava/TestMaven.java
@@ -0,0 +1,11 @@
+package stepik.javacore.libraryjava;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class TestMaven {
+    public static void main(String[] args) {
+        File tempDirectory = FileUtils.getTempDirectory();
+    }
+}
