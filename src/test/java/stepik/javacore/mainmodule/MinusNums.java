3d3bef941f0bba70b9d06fdf5655897413a90b2c
Costa Varyn <d3tug@yandex.ru>
2022-11-10 11:29:02 +0300

    updated MinusNums.java

--- src/test/java/stepik/javacore/mainmodule/MinusNums.java
+++ src/test/java/stepik/javacore/mainmodule/MinusNums.java
@@ -1,18 +1,38 @@
 package stepik.javacore.mainmodule;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class MinusNums {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int min = 0;
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        //sc.next();
+        String s2 = sc.next();
+        sc.close();
+        System.out.println(minus(s1, s2));
+    }
 
-        int max = 1000000000;
+    public static String minus(String s11, String s22) {
+        StringBuilder buf = new StringBuilder();
+        for (int i1 = s11.length() - 1, i2 = s22.length() - 1, carry = 0;
+             i1 >= 0 || i2 >= 0 || carry != 0;
+             i1--, i2--) {
+            int digit1 = i1 < 0 ? 0 :
+                    Integer.parseInt(Character.toString(s11.charAt(i1)));
+            int digit2 = i2 < 0 ? 0 :
+                    Integer.parseInt(Character.toString(s22.charAt(i2)));
 
-        if ((min <= a && a <= max) && (min <= b && b <= max)) {
-            System.out.println(a - b);
+            int digit = digit1 - digit2 - carry;
+            if (digit < 0) {
+                carry = 1;
+                digit -= 10;
+            } else {
+                carry = 0;
+            }
+
+            buf.append(digit);
         }
+        return buf.reverse().toString();
     }
 }
