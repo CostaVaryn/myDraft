f0477c79ef32ea7f355816c68ae047960c64bc5b
Costa Varyn <d3tug@yandex.ru>
2022-11-10 16:09:49 +0300

    updated ConditionalKamaz.java

--- src/test/java/stepik/javabasics/block5/ConditionalKamaz.java
+++ src/test/java/stepik/javabasics/block5/ConditionalKamaz.java
@@ -11,7 +11,7 @@ public class ConditionalKamaz {
         int sumMas = xMasKamaz + yMasGruz;
         if (sumMas > maxMas) {
             System.out.println("Превышена максимальная грузоподъёмность!");
-        } else if ((sumMas < maxMas) && (sumMas > 12000)) {
+        } else if (sumMas > 12000) {
             System.out.println("Мост рухнет!");
         } else {
             System.out.println("Ура доехали!");
