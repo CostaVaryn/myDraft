78093549dc6b7037423645f5dfd5afc2ddf69890
Costa Varyn <d3tug@yandex.ru>
2022-11-10 11:29:11 +0300

    added ArrSimpleNums.java

---
+++ src/test/java/stepik/javacore/mainmodule/ArrSimpleNums.java
@@ -0,0 +1,24 @@
+package stepik.javacore.mainmodule;
+
+import java.util.Scanner;
+
+public class ArrSimpleNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0, num = 0;
+        sc.close();
+        for (int i = 1; i <= 1000000; i++) {
+            if (i > 3 && ((i % 2 == 0) || (i % 3 == 0)) || i == 1) {
+                continue;
+            } else {
+                num = i;
+                count++;
+            }
+            if (count == n) {
+                System.out.println(num);
+                break;
+            }
+        }
+    }
+}
