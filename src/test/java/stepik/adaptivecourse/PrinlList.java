c534c1e811dd953deb0772f4570268d079fc4f49
Costa Varyn <d3tug@yandex.ru>
2022-11-11 11:01:47 +0300

    added PrinlList.java

---
+++ src/test/java/stepik/adaptivecourse/PrinlList.java
@@ -0,0 +1,19 @@
+package stepik.adaptivecourse;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class PrinlList {
+    public static void main(String[] args) {
+        List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+        for(String s : nameList) {
+            System.out.println(s);
+        }
+
+        List<String> set = new ArrayList<>(Arrays.asList("Alpha", "Gamma", "Omega"));
+        System.out.println(set);
+
+    }
+}
