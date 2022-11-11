4af07bc15e4e560b9a01d15aba9ab5fcdccb8bd7
Costa Varyn <d3tug@yandex.ru>
2022-11-11 11:10:30 +0300

    updated HashSetStr.java

--- src/test/java/stepik/adaptivecourse/HashSetStr.java
+++ src/test/java/stepik/adaptivecourse/HashSetStr.java
@@ -2,21 +2,15 @@ package stepik.adaptivecourse;
 
 import java.util.*;
 
-import static java.util.Arrays.*;
-
 public class HashSetStr {
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-
-        //String str = sc.nextLine();
-        String str = "java scala java clojure clojure";
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         String[] arStr = str.split(" ");
 
         List list = new ArrayList(Arrays.asList(arStr));
-
         Collections.sort(list);
         Set resultSet = new LinkedHashSet(list);
-
 
         //HashSet<String> hashSet = new HashSet<>(Arrays.asList(arStr));
 
@@ -25,3 +19,23 @@ public class HashSetStr {
         }
     }
 }
+
+/*
+Write a lambda expression that accepts a list of strings and returns new list of distinct strings (without repeating).
+The order of elements in the result list may be any (elements will be sorted by the testing system).
+
+If the input list doesn't contain any strings then the result list should be empty.
+Hints: it is possible to use sets, maps, lists and so on for helping.
+Solution format. Submit your lambda expression in any valid format with ; on the end.
+Examples: x -> x; (x) -> { return x; };
+
+Sample Input 1:
+java scala java clojure clojure
+Sample Output 1:
+clojure java scala
+
+Sample Input 2:
+the three the three the three an an a
+Sample Output 2:
+a an the three
+ */
