c8b0057319c2aad5627b558cc9bc655b7dea7d19
Costa Varyn <d3tug@yandex.ru>
2022-11-14 13:56:52 +0300

    updated MapGiven.java

--- src/test/java/stepik/adaptivecourse/block3/MapGiven.java
+++ src/test/java/stepik/adaptivecourse/block3/MapGiven.java
@@ -5,19 +5,13 @@ import java.util.TreeMap;
 
 public class MapGiven {
     public static void main(String[] args) {
-
-
         Map<String, Integer> map = new TreeMap<>();
         map.put("Gamma",  3);
         map.put("Omega", 24);
         map.put("Alpha",  1);
-
         for (Map.Entry entry: map.entrySet()) {
-
             System.out.println(entry);
-
         }
-
     }
 }
 
