20f8b07b63139fa632d85e838526c96021fcfcca
Costa Varyn <d3tug@yandex.ru>
2022-11-14 14:13:41 +0300

    updated TestScanner.java

--- src/test/java/miniclasses/TestScanner.java
+++ src/test/java/miniclasses/TestScanner.java
@@ -7,56 +7,41 @@ public class TestScanner {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.print("Введите число: ");
 
         if (scanner.hasNextInt()) {
             int number = scanner.nextInt();
             // Диапазон значений от -2^31 до 2^31
             System.out.println("Спасибо! Вы ввели 32-х битное значение " + number);
-        }
-
-        if (scanner.hasNextBigInteger()) {
+        } else if (scanner.hasNextBigInteger()) {
             BigInteger bigNumber = scanner.nextBigInteger();
             // Аналог целочисленных значений произвольной длины, у которого нет ограничения в 64 битов
             System.out.println("Ух ты! Вы ввели большое значение " + bigNumber);
-        }
-
-        if (scanner.hasNextLong()) {
+        } else if (scanner.hasNextLong()) {
             long longValue = scanner.nextLong();
             // Диапазон значений от -2^63 до 2^63
             System.out.println("Вау! Вы ввели 64-х битное значение " + longValue);
-        }
-
-        if (scanner.hasNextByte()) {
+        } else if (scanner.hasNextByte()) {
             byte byteValue = scanner.nextByte();
             // Диапазон значений от -128 до 127
             System.out.println("Вы ввели 8-ми битное значение " + byteValue);
-        }
-
-        if (scanner.hasNextFloat()) {
+        } else if (scanner.hasNextFloat()) {
             float floatValue = scanner.nextFloat();
             System.out.println("Вы ввели 32-х битное число с плавающей точкой " + floatValue);
-        }
-
-        if (scanner.hasNextDouble()) {
+        } else if (scanner.hasNextDouble()) {
             double doubleValue = scanner.nextDouble();
             System.out.println("Вы ввели 64-х битное число с плавающей точкой " + doubleValue);
-        }
-
-        if (scanner.hasNextShort()) {
+        } else if (scanner.hasNextShort()) {
             short shortValue = scanner.nextShort();
             // Диапазон значений от -32768 до 32767
             System.out.println("Вы ввели 16-ти битное значение " + shortValue);
-        }
-        if (scanner.hasNextDouble()) {
+        } else if (scanner.hasNextDouble()) {
             double doubleValue = scanner.nextDouble();
             System.out.println("Вы ввели 64-х битное число с плавающей точкой " + doubleValue);
-        }
-        if (scanner.hasNextBoolean()) {
+        } else if (scanner.hasNextBoolean()) {
             boolean booleanValue = scanner.nextBoolean();
             System.out.println("Вы использовали значение типа \"истина/ложь\" " + booleanValue);
-        }
-        else {
+        } else {
             System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
         }
     }
