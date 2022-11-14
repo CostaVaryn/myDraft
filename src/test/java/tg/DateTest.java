5ca2a88c69ddcad2bc06daccca33fc9e1728a34e
Costa Varyn <d3tug@yandex.ru>
2022-11-14 12:09:18 +0300

    added DateTest.java

---
+++ src/test/java/tg/DateTest.java
@@ -0,0 +1,18 @@
+package tg;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTest {
+    public static void main(String[] args) {
+        String date = "14.11.2022";
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date dayWeek = null;
+        try {
+            dayWeek = format.parse(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(new SimpleDateFormat("EEEE").format(dayWeek));
+    }
+}
