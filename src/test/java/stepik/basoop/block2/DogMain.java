4bcae5606f8592b7c6727811e5246ae83fcd7559
Costa Varyn <d3tug@yandex.ru>
2022-11-15 14:18:16 +0300

    updated DogMain.java

--- src/test/java/stepik/basoop/block2/DogMain.java
+++ src/test/java/stepik/basoop/block2/DogMain.java
@@ -2,8 +2,16 @@ package stepik.basoop.block2;
 
 public class DogMain {
     public static void main(String[] args) {
-        Dog dog = new Dog("Mastif", "big", 4);
-        dog.makeVoice();
+        //Dog dog = new Dog("Mastif", "big", 4);
+        //dog.makeVoice();
+        try {
+            Dog dog1 = new Dog("Mastif", "big", 4);
+            dog1.getInfo();
+            Dog dog2 = new Dog("Freebreed", "big", -2);
+            dog2.getInfo();
+        } catch (NegativeAgeException e){
+            System.out.println(e.toString());
+        }
         Cat cat = new Cat("Freebreed", "small", 3);
         cat.makeVoice();
     }
@@ -17,15 +25,20 @@ class Dog implements Voiceable {
     private String size;
     private int age;
 
-    Dog(String breed, String size, int age) {
+    Dog(String breed, String size, int age) throws NegativeAgeException {
         this.breed = breed;
         this.size = size;
+        if(age < 0)
+            throw new NegativeAgeException(age);
         this.age = age;
     }
 
     @Override
     public void makeVoice() {
         System.out.println("Gav Gav Gav");
+    }
+    void getInfo(){
+        System.out.println("breed:" + breed + ", size:" + size+ ", age:" + age);
     }
 }
 
