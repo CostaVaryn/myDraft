17bc08e19de6abbb5656bbbf4b889c1a1db194e0
Costa Varyn <d3tug@yandex.ru>
2022-11-15 14:00:52 +0300

    updated SortMinMax.java

--- src/test/java/stepik/basoop/block1/SortMinMax.java
+++ src/test/java/stepik/basoop/block1/SortMinMax.java
@@ -6,18 +6,16 @@ import java.util.Scanner;
 public class SortMinMax {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        int n;
-        n = in.nextInt();
+        int n = in.nextInt();
         double[] array = new double[n];
         double max, min;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++) {
             array[i] = in.nextDouble();
         }
         in.close();
         Arrays.sort(array);
         /*
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++) {
            ....
         }*/
         min = array[0];
