c391bb9b90a6d933b79c894285c64a383dcbed92
Costa Varyn <d3tug@yandex.ru>
2022-11-15 14:00:48 +0300

    updated SimpNumDivide.java

--- src/test/java/stepik/basoop/block1/SimpNumDivide.java
+++ src/test/java/stepik/basoop/block1/SimpNumDivide.java
@@ -2,9 +2,7 @@ package stepik.basoop.block1;
 
 public class SimpNumDivide {
     public static void main(String[] args) {
-
         int number = 42;
-
         for(int i = 1; i <= number; i++) {
             if(number % i == 0) {
                 System.out.println(i);
