c30e7cbe013c29192fe6503a1520f3c81dbfd85b
Costa Varyn <d3tug@yandex.ru>
2022-11-15 14:00:39 +0300

    updated ConvertMain.java

--- src/test/java/stepik/basoop/block1/ConvertMain.java
+++ src/test/java/stepik/basoop/block1/ConvertMain.java
@@ -4,24 +4,21 @@ import java.util.Scanner;
 
 public class ConvertMain {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
         ConverterString converter = new ConverterString();
         String str, strUpper, strLower;
         str=in.nextLine();
         strUpper=converter.toUpperCase(str);
         strLower=converter.toLowerCase(str);
-
         System.out.println(strUpper);
         System.out.println(strLower);
     }
 }
 class ConverterString {
-
-    String toLowerCase(String str){
+    String toLowerCase(String str) {
         return str.toLowerCase();
     }
-    String toUpperCase(String str){
+    String toUpperCase(String str) {
          return str.toUpperCase();
     }
 }
