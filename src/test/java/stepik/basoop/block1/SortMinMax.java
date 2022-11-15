70f45a67550a5a2df00da9ad13768f20635d33f9
Costa Varyn <d3tug@yandex.ru>
2022-11-15 13:28:59 +0300

    added SortMinMax.java

---
+++ src/test/java/stepik/basoop/block1/SortMinMax.java
@@ -0,0 +1,28 @@
+package stepik.basoop.block1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortMinMax {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n;
+        n = in.nextInt();
+        double[] array = new double[n];
+        double max, min;
+        for(int i=0;i<n;i++){
+            array[i] = in.nextDouble();
+        }
+        in.close();
+        Arrays.sort(array);
+        /*
+        for(int i=0;i<n;i++){
+           ....
+        }*/
+        min = array[0];
+        max = array[n-1];
+        System.out.println(min);
+        System.out.println(max);
+    }
+}
