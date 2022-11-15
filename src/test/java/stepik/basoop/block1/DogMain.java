6254c760a3b8e1a78b2d35f7bf989060f6a33457
Costa Varyn <d3tug@yandex.ru>
2022-11-15 15:23:33 +0300

    updated DogMain.java

--- src/test/java/stepik/basoop/block1/DogMain.java
+++ src/test/java/stepik/basoop/block1/DogMain.java
@@ -19,36 +19,40 @@ public class DogMain {
         calories = dog2.eat(6,6,8);
         result = dog2.checkCalories(calories);
 
+       // float norma = (535 / 10) * 2.5f;
+       // float rez = norma - calories;
+       // System.out.println(rez);
+
         System.out.println(result);
     }
 
 }
 class Dog{
 
-    String breed;
-    String size;
-    int age;
-    float weight;
+    private String breed;
+    private String size;
+    private int age;
+    private float weight = 5.0f;
 
-    Dog(String breed, String size, int age,float weight){
+    Dog(String breed, String size, int age,float weight) {
         this.breed = breed;
         this.size = size;
         this.age = age;
-        this.weight = 5.0F;
+        this.weight = weight;
     }
 
-    void getInfo(){
+    void getInfo() {
         System.out.println("breed:" + breed +", size:" + size + ", age:" + age);
     }
     int eat(int carbohydrates, int squirrels, int fats) {
         return carbohydrates * 4 + squirrels * 4 + fats * 9;
     }
-    String checkCalories(int calories){
-        float weiCal = (calories/10)*weight;
-        float rez = weiCal - calories;
+    String checkCalories(int calories) {
+        float norma = (float)(535 / 10) * weight;
+        float rez = norma - calories;
         if ((rez >= -30) && (rez <= 30)) {
             return "good";
-        } else if (rez < 30) {
+        } else if (rez > 30) {
             return "bad";
         } else if (rez < -30) {
             return "overate";
