12d38001cb4663c705a1f23e888ce6eb75d7c2d2
Costa Varyn <d3tug@yandex.ru>
2022-11-15 14:00:43 +0300

    updated MiniDate.java

--- src/test/java/stepik/practicejavabasic/block2/MiniDate.java
+++ src/test/java/stepik/practicejavabasic/block2/MiniDate.java
@@ -9,11 +9,9 @@ public class MiniDate {
         int sec = s % 60;
         int min = (s / 60) % 60;
         int hours = (s / 60) / 3600;
-
         String strSec = (sec < 10) ? "0" + Integer.toString(sec) : Integer.toString(sec);
         String strMin = (min < 10) ? "0" + Integer.toString(min) : Integer.toString(min);
         String strHours = (hours < 10) ? "" + Integer.toString(hours) : Integer.toString(hours);
-
         System.out.println(strHours + ":" + strMin + ":" + strSec);
     }
 }
