24361b1f9ca674009bfda9edc792ab42e9fa6353
Costa Varyn <d3tug@yandex.ru>
2022-11-16 15:46:15 +0300

    added SumEvenNums.java

---
+++ src/test/java/stepik/practicejavabasic/block4/SumEvenNums.java
@@ -0,0 +1,36 @@
+package stepik.practicejavabasic.block4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumEvenNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        if (n <= 0) {
+            sc.close();
+        } else {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+                if(arr[i] % 2 == 0) {
+                    sum += arr[i];
+                }
+            }
+            sc.close();
+            System.out.println(sum);
+        }
+    }
+}
+
+/*
+Напишите программу, которая должна получить на вход размерность массива больше нуля
+ и сам массив значений int и вернуть сумму четных чисел. Если четных чисел в массиве нет, сумма равна 0.
+
+Sample Input:
+5
+23 25 27 29 31
+Sample Output:
+0
+ */
