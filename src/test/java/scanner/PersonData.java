5aa57af121bdd138efe8b17e9a95005cfbc980b6
Costa Varyn <d3tug@yandex.ru>
2022-11-16 11:09:10 +0300

    updated PersonData.java

--- src/test/java/scanner/PersonData.java
+++ src/test/java/scanner/PersonData.java
@@ -3,7 +3,6 @@ package scanner;
 import java.util.Scanner;
 
 public class PersonData {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input name: ");
