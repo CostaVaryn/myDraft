543e98ebaebec91785ec455bd931868cb76501e5
Costa Varyn <d3tug@yandex.ru>
2022-11-16 15:09:37 +0300

    added AvgNumFromArrays.java

---
+++ src/test/java/stepik/practicejavabasic/block3/AvgNumFromArrays.java
@@ -0,0 +1,31 @@
+package stepik.practicejavabasic.block3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AvgNumFromArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+        String[] arrStr = str.replaceAll("0","").split(" ");
+        Arrays.sort(arrStr);
+        System.out.println(arrStr[(arrStr.length - 1) / 2]);
+    }
+}
+
+/*
+Напишите программу, которая считывает последовательность целочисленных значений из стандартного ввода и находит среднее значение.
+
+Подробности:
+
+Вы должны считывать значения последовательности, пока следующее не станет 0.
+Нулевое значение означает конец входной последовательности и не является ее частью.
+Последовательность гарантированно содержит хотя бы одно значение.
+Среднее значение также является целым числом. Используйте целочисленные операции.
+
+Sample Input:
+1 2 3 4 5 0
+Sample Output:
+3
+ */
