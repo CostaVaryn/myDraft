32c529c1fb20dafed8b8d892d2e096e71091a7c4
Costa Varyn <d3tug@yandex.ru>
2022-11-17 13:47:26 +0300

    updated WriterTest.java

--- src/test/java/writer/WriterTest.java
+++ src/test/java/writer/WriterTest.java
@@ -5,9 +5,8 @@ import java.io.IOException;
 
 public class WriterTest {
     public static void main(String[] args) throws IOException {
-
         String data = "Test data";
-        FileOutputStream out = new FileOutputStream("C:\\Users\\cosva\\Desktop\\testFile.txt");
+        FileOutputStream out = new FileOutputStream("C:\\Users\\...\\Desktop\\testFile.txt");
         out.write(data.getBytes());
         out.close();
     }
