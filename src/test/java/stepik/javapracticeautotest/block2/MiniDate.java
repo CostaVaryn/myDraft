2531e271546a0e9c645d06a1848fa7da51372b28
Costa Varyn <d3tug@yandex.ru>
2022-11-17 13:38:30 +0300

    updated MiniDate.java

--- src/test/java/stepik/javapracticeautotest/block2/MiniDate.java
+++ src/test/java/stepik/javapracticeautotest/block2/MiniDate.java
@@ -6,12 +6,14 @@ public class MiniDate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int s = scanner.nextInt();
-        int sec = s % 60;
-        int min = (s / 60) % 60;
-        int hours = (s / 60) / 3600;
-        String strSec = (sec < 10) ? "0" + Integer.toString(sec) : Integer.toString(sec);
-        String strMin = (min < 10) ? "0" + Integer.toString(min) : Integer.toString(min);
-        String strHours = (hours < 10) ? "" + Integer.toString(hours) : Integer.toString(hours);
-        System.out.println(strHours + ":" + strMin + ":" + strSec);
+        if (s > 0 && s < 86400) {
+            int sec = s % 60;
+            int min = (s / 60) % 60;
+            int hours = s / 3600;
+            String strSec = (sec < 10) ? "0" + sec : Integer.toString(sec);
+            String strMin = (min < 10) ? "0" + min : Integer.toString(min);
+            String strHours = (hours < 10) ? "" + hours : Integer.toString(hours);
+            System.out.println(strHours + ":" + strMin + ":" + strSec);
+        }
     }
 }
