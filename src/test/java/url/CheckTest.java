bcccb865f9d41328670e7669d969053803abec8f
Costa Varyn <d3tug@yandex.ru>
2022-11-17 13:46:48 +0300

    updated CheckTest.java

--- src/test/java/url/CheckTest.java
+++ src/test/java/url/CheckTest.java
@@ -7,12 +7,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CheckTest {
-
     public static void main(String[] args) throws IOException {
         String image = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
         URL url = new URL(image);
         InputStream input = url.openStream();
-
         Path path = Paths.get("C:\\GoogleLogo.png");
         Files.copy(input, path);
     }
