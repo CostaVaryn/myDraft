69fcdea443357fcb85d7dad0e7a873c52e8ba419
Costa Varyn <d3tug@yandex.ru>
2022-11-18 11:05:51 +0300

    updated ForNumLadder.java

--- src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumLadder.java
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumLadder.java
@@ -6,15 +6,16 @@ public class ForNumLadder {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        in.close();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j+(j==i?"\n":" "));
+                System.out.print(j + (j == i ? "\n" : " "));
             }
         }
     }
 }
 
-/**
+/*
  * Напечатайте "лесенку" из чисел от единицы до n, n > 0.
  * Примечание. Каждая строка в "лесенке" заканчивается цифрой (не пробелом).
  *
