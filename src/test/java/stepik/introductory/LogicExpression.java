adec9a9f88347250d08bd1d2ba1b4e11c5698bd5
Costa Varyn <d3tug@yandex.ru>
2022-11-18 11:06:39 +0300

    updated LogicExpression.java

--- src/test/java/stepik/introductory/LogicExpression.java
+++ src/test/java/stepik/introductory/LogicExpression.java
@@ -6,7 +6,6 @@ public class LogicExpression {
         boolean b = false;
         int c = 30;
         int q = 5;
-
         boolean r = (a || b && c >= 25) || (c < 20 * q) && !(q == 5);
         System.out.println(r);
     }
