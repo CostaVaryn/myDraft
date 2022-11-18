6c10867470946ff7cfe4745ca892765c765b1feb
Costa Varyn <d3tug@yandex.ru>
2022-11-18 11:05:33 +0300

    updated ForAlphabetWords.java

--- src/test/java/stepik/introductory/scanner/cycles/justfor/ForAlphabetWords.java
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForAlphabetWords.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class ForAlphabetWords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String x = sc.nextLine(); String[] y = sc.nextLine().split(" ");
-        for(int i = 0; i < y.length; i++) {
-            if(y[i].charAt(0) >= x.charAt(0) && y[i].charAt(0) <= x.charAt(2))
-                System.out.println(y[i]);
-        }
+        String x = sc.nextLine();
+        String[] y = sc.nextLine().split(" ");
         sc.close();
+        for (String s : y) {
+            if (s.charAt(0) >= x.charAt(0) && s.charAt(0) <= x.charAt(2))
+                System.out.println(s);
+        }
     }
 }
 
-/**
+/*
  * На вход подаётся строка с двумя буквами, а затем - строка, состоящая из слов.
  * Выведите на печать все слова, которые начинаются на буквы, идущие по алфавиту между буквами с первой строки (включительно).
  * Вывести слова необходимо в том же порядке, в котором они подаются на вход.
