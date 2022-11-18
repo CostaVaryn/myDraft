e9f3aa3252ef47a1fb2db6b5289e208ab314aee1
Costa Varyn <d3tug@yandex.ru>
2022-11-18 11:06:10 +0300

    updated ForNumSqrt.java

--- src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumSqrt.java
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForNumSqrt.java
@@ -9,8 +9,8 @@ public class ForNumSqrt {
             if (sc.hasNextInt()) {
                 int n = sc.nextInt();
                 for (int i = 2; i < 6; i++) {
-                    int pow= (int) Math.pow(n,i);
-                    System.out.print(pow + (i<5 ? " " : ""));
+                    int pow = (int) Math.pow(n,i);
+                    System.out.print(pow + (i < 5 ? " " : ""));
                 }
                 System.out.print("\n");
             }
@@ -19,7 +19,7 @@ public class ForNumSqrt {
     }
 }
 
-/**
+/*
  * На вход подаётся несколько целых чисел. Выведите эти числа, возведённые в степени от двух до пяти.
  * Формат вывода: для каждого введённого числа степени выводятся в отдельной строке через пробел.
  * Примечание. Каждая строка вывода оканчивается числом, а не пробелом.
