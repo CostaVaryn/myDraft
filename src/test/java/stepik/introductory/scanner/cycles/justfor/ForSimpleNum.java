6eabce3c3f9464bdcf6c35fefc7eb23ac470caab
Costa Varyn <d3tug@yandex.ru>
2022-11-18 11:06:27 +0300

    updated ForSimpleNum.java

--- src/test/java/stepik/introductory/scanner/cycles/justfor/ForSimpleNum.java
+++ src/test/java/stepik/introductory/scanner/cycles/justfor/ForSimpleNum.java
@@ -6,6 +6,7 @@ public class ForSimpleNum {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int a = Math.abs(sc.nextInt());
+        sc.close();
         int b = 0;
         for (int i = 1; i < a; i++) {
             if (a % i == 0) {
