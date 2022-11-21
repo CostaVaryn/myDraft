242949f7c09099e63290488b0932271c66f62568
Costa Varyn <d3tug@yandex.ru>
2022-11-21 11:10:24 +0300

    updated ArrOneThreeTwo.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrOneThreeTwo.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrOneThreeTwo.java
@@ -6,21 +6,21 @@ public class ArrOneThreeTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
+        sc.close();
         String[] num = line.split(" ");
-        System.out.print(num[0]+ " ");
-        for (int i = 1; i < num.length-2; i+=2) {
-            System.out.print(num[i+1] + " " + num[i] + " ");
+        System.out.print(num[0] + " ");
+        for (int i = 1; i < num.length - 2; i += 2) {
+            System.out.print(num[i + 1] + " " + num[i] + " ");
         }
-        if (num.length%2==0) {
-            System.out.print(num[num.length-1]);
-        }
-        else {
-            System.out.print(num[num.length-2] + " " + num[num.length-1]);
+        if (num.length % 2 == 0) {
+            System.out.print(num[num.length - 1]);
+        } else {
+            System.out.print(num[num.length - 2] + " " + num[num.length - 1]);
         }
     }
 }
 
-/**
+/*
  * На ввод подаётся последовательность целых чисел.
  * Поменяйте местами соседние элементы последовательности (второй с третьим, четвёртый с пятым и т.д.),
  * кроме первого и последнего.
