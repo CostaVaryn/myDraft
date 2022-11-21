f617e1bc94be3b7778065a1f976ee3a23f12e1b0
Costa Varyn <d3tug@yandex.ru>
2022-11-21 11:10:41 +0300

    updated ArrUpendMatrix.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrUpendMatrix.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrUpendMatrix.java
@@ -13,46 +13,48 @@ public class ArrUpendMatrix {
             str = str + str1 + " ";
             counter++;
         }
+        sc.close();
         String[] arr = str.split(" ");
-        String[][] matrix = new String[counter][arr.length/counter];
+        String[][] matrix = new String[counter][arr.length / counter];
         for (int i = 0 ; i < counter; i++){
-            for (int j = 0 ; j < (arr.length/counter); j++) {
+            for (int j = 0 ; j < (arr.length / counter); j++) {
                 matrix[i][j] = arr[k];
                 k++;
             }
         }
-        String[][] matrix2 = new String[arr.length/counter][counter];
+        String[][] matrix2 = new String[arr.length / counter][counter];
         for (int x = 0; x < counter ; x++) {
-            for (int y = (arr.length/counter)-1; y >= 0; y--) {
-                matrix2[y][x] = arr[k-1];
+            for (int y = (arr.length / counter)-1; y >= 0; y--) {
+                matrix2[y][x] = arr[k - 1];
                 k--;
             }
         }
-        for (int m = 0; m < (arr.length/counter); m++) {
+        for (int m = 0; m < (arr.length / counter); m++) {
             for (int n = 0; n < counter; n++) {
-                if (n == (counter-1))
+                if (n == (counter - 1)) {
                     System.out.print(matrix2[m][n]);
-                else
+                } else {
                     System.out.print(matrix2[m][n] + " ");
+                }
             }
             System.out.println();
         }
 
-        /**
+        /*
          String[] arr = str.split(" ");
-         int b = arr.length/counter;
+         int b = arr.length / counter;
          String[][] matrix = new String[b][counter];
             for (int i = 0 ; i < counter; i++) {
-                for (int j = 0 ; j < (arr.length/counter); j++) {
-                    matrix[j][counter-1-i] = arr[k];
+                for (int j = 0 ; j < (arr.length / counter); j++) {
+                    matrix[j][counter - 1 - i] = arr[k];
                     k++;
                 }
             }
-         for (String[]i: matrix) {
+         for (String[] i : matrix) {
             int z = 1;
-            for(String j:i) {
+            for (String j : i) {
                 System.out.print(j);
-                if(counter>z) {
+                if (counter > z) {
                     System.out.print(" ");
                     z++;
                 }
@@ -61,11 +63,10 @@ public class ArrUpendMatrix {
          }
          */
 
-
     }
 }
 
-/**
+/*
  * На вход подаётся матрица (двумерный массив). Все элементы матрицы - целые числа.
  * Поверните матрице на 90 градусов по часовой стрелке.
  *
