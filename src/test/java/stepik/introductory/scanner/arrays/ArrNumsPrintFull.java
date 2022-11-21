40ebf48c417f7d20e12574fce2db23b4449550e3
Costa Varyn <d3tug@yandex.ru>
2022-11-21 11:10:08 +0300

    updated ArrNumsPrintFull.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrNumsPrintFull.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrNumsPrintFull.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 
 public class ArrNumsPrintFull {
     public static void main(String[] args) {
-        Scanner sc= new Scanner (System.in);
-        String str= sc.nextLine();
+        Scanner sc = new Scanner (System.in);
+        String str = sc.nextLine();
+        sc.close();
         String[] arrayString = str.split(" ");
         int number = arrayString.length;
-        int arr[] = new int[number];
-        for(int i = 0; i < arrayString.length; i++) {
-            arr[i] = Integer.valueOf(arrayString[i]);
+        int[] arr = new int[number];
+        for (int i = 0; i < arrayString.length; i++) {
+            arr[i] = Integer.parseInt(arrayString[i]);
         }
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
 
-/**
+/*
  * На вход подаётся последовательность целых чисел.
  * Создайте из этой последовательности массив чисел, отсортированный по возрастанию, и выведите на печать.
  *
