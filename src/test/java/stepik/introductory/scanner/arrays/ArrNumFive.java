562fba732bcb217339f5a2da7dd6a80056ed8a7b
Costa Varyn <d3tug@yandex.ru>
2022-11-21 11:09:40 +0300

    updated ArrNumFive.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrNumFive.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrNumFive.java
@@ -6,7 +6,8 @@ public class ArrNumFive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] nums = new int[n+1];
+        sc.close();
+        int[] nums = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
             nums[i] = nums[i] + i;
