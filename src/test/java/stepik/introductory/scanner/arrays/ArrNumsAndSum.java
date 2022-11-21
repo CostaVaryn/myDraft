22cd5e8e8c7556b8b5430fd3750f8242fb5e55cf
Costa Varyn <d3tug@yandex.ru>
2022-11-21 11:09:47 +0300

    updated ArrNumsAndSum.java

--- src/test/java/stepik/introductory/scanner/arrays/ArrNumsAndSum.java
+++ src/test/java/stepik/introductory/scanner/arrays/ArrNumsAndSum.java
@@ -6,20 +6,21 @@ public class ArrNumsAndSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int arr [] = new int[x+1];
+        sc.close();
+        int[] arr = new int[x + 1];
         int c = 0;
         if (x > 1) {
-            for (int i = 2; i<=x; i+=2) {
+            for (int i = 2; i <= x; i += 2) {
                 arr[i] = (i);
                 c = c + arr[i];
-                System.out.print(i != arr.length-1 ? arr[i]+" " : arr[i]);
+                System.out.print(i != arr.length - 1 ? arr[i] + " " : arr[i]);
             }
-            System.out.print("\n"+c);
+            System.out.print("\n" + c);
         }
     }
 }
 
-/**
+/*
  * На вход подаётся целое число n > 1. Сформируйте массив чётных чисел от 2 до n (включительно).
  * Выведите на печать массив в одной строке, через пробел.
  * Посчитайте и выведите на печать сумму элементов массива.
