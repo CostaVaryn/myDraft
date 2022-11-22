59f5e0853048a482d89be4b8ba78c3c780f223b0
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:30:14 +0300

    updated MathSinCos.java

--- src/test/java/stepik/introductory/scanner/cycles/basicmath/MathSinCos.java
+++ src/test/java/stepik/introductory/scanner/cycles/basicmath/MathSinCos.java
@@ -6,12 +6,13 @@ public class MathSinCos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
+        sc.close();
         double y = Math.sin(Math.toRadians(a)) + Math.cos(Math.toRadians(a));
         System.out.println(y);
     }
 }
 
-/**
+/*
  * Дан угол A градусов. Выведите  y = sin(A) + cos(A).
  *
  * Sample Input:
