c42c10cf61f28eab0485f03f5c1c8970e1119ff5
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:31:08 +0300

    updated ScanDivideAndModulo.java

--- src/test/java/stepik/introductory/scanner/basconcepts/ScanDivideAndModulo.java
+++ src/test/java/stepik/introductory/scanner/basconcepts/ScanDivideAndModulo.java
@@ -6,6 +6,7 @@ public class ScanDivideAndModulo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int budget = sc.nextInt(), salary = sc.nextInt();
+        sc.close();
         int maxPeople = budget / salary;
         int restBalance = budget % salary;
         System.out.println(maxPeople + " " + restBalance);
