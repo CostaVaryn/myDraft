a45e0b4666884129024e50eb800a743d804386ba
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:31:13 +0300

    updated ScanDraft.java

--- src/test/java/stepik/introductory/scanner/basconcepts/ScanDraft.java
+++ src/test/java/stepik/introductory/scanner/basconcepts/ScanDraft.java
@@ -14,6 +14,7 @@ public class ScanDraft {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int value = sc.nextInt();
+            sc.close();
             int myVar = value % 10; // результат 5, т.к. остаток берется целочисленный от 36,5 остаток целое число 5
             System.out.print(myVar);
     }
