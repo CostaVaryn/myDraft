899b01dcd97d34f861e2a5bd5d4b90f364edb2ad
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:31:20 +0300

    updated ScanEquation.java

--- src/test/java/stepik/introductory/scanner/basconcepts/ScanEquation.java
+++ src/test/java/stepik/introductory/scanner/basconcepts/ScanEquation.java
@@ -6,6 +6,7 @@ public class ScanEquation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
+        sc.close();
         double y = 5 * (x * x) + 2 * x + 11;
         System.out.println(y);
     }
