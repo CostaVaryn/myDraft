74cdf7eb177dd3a5d70760d6a833d0c73d401646
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:29:48 +0300

    updated MathNumToRadians.java

--- src/test/java/stepik/introductory/scanner/cycles/basicmath/MathNumToRadians.java
+++ src/test/java/stepik/introductory/scanner/cycles/basicmath/MathNumToRadians.java
@@ -6,6 +6,7 @@ public class MathNumToRadians {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
+        sc.close();
         System.out.println(Math.toRadians(x));
     }
 }
