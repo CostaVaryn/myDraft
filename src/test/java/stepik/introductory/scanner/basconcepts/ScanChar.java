d5f4a12ee507606e30fbdbc2e9a811e0404e62a3
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:31:03 +0300

    updated ScanChar.java

--- src/test/java/stepik/introductory/scanner/basconcepts/ScanChar.java
+++ src/test/java/stepik/introductory/scanner/basconcepts/ScanChar.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class ScanChar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String str1 = sc.next(), str2 = sc.next();
+        sc.close();
         System.out.println(str1.charAt(0) < str2.charAt(0));
-
         int x = 5, y = 3;
         int z = ++x;
         y += x--;
