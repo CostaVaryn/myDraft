9a84e05a98e7601af524e5b51a8446a041d43b59
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:31:25 +0300

    updated ScanEquationRoots.java

--- src/test/java/stepik/introductory/scanner/basconcepts/ScanEquationRoots.java
+++ src/test/java/stepik/introductory/scanner/basconcepts/ScanEquationRoots.java
@@ -7,16 +7,16 @@ public class ScanEquationRoots {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
         sc.close();
-        double sum = (-1*b)/a;
-        double mult = c/a;
-        System.out.println(sum + " " +mult);
+        double sum = (-1 * b) / a;
+        double mult = c / a;
+        System.out.println(sum + " " + mult);
     }
 }
 
-/**
- * Sample Input:
- * 1 2 -4
- *
- * Sample Output:
- * -2.0 -4.0
+/*
+Sample Input:
+1 2 -4
+
+Sample Output:
+-2.0 -4.0
  */
