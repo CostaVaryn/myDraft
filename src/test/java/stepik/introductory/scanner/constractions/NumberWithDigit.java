fa1d388169b5404a4beead21c38944ea4d707905
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:30:41 +0300

    updated NumberWithDigit.java

--- src/test/java/stepik/introductory/scanner/constractions/NumberWithDigit.java
+++ src/test/java/stepik/introductory/scanner/constractions/NumberWithDigit.java
@@ -6,17 +6,16 @@ public class NumberWithDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = Math.abs(sc.nextInt()), y = Math.abs(sc.nextInt());
-        int z = (x%100)/10;
+        int z = (x % 100) / 10;
+        sc.close();
         if (x > 99 && x < 1000) {
             if (z == y) {
                 System.out.println("true");
             } else {
                 System.out.println("false");
             }
-
         } else {
             System.out.println("error");
         }
-        sc.close();
     }
 }
