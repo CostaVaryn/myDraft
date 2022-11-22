329559f440926e6af6fd7b49fad3371e6730a01f
Costa Varyn <d3tug@yandex.ru>
2022-11-22 09:30:06 +0300

    updated MathRectangle.java

--- src/test/java/stepik/introductory/scanner/cycles/basicmath/MathRectangle.java
+++ src/test/java/stepik/introductory/scanner/cycles/basicmath/MathRectangle.java
@@ -7,13 +7,14 @@ public class MathRectangle {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         double b = sc.nextDouble();
+        sc.close();
         double c = Math.pow((Math.pow(a,2) + Math.pow(b,2)), 1.0/2.0);
         double res = a + b + c;
-        System.out.println(Math.round(res)*1.0);
+        System.out.println(Math.round(res) * 1.0);
     }
 }
 
-/**
+/*
  * На вход подаются длины двух катетов прямоугольного треугольника a и b.
  * Посчитайте и выведите на печать периметр этого треугольника.
  * Значение периметра необходимо округлить до целого (в математическом смысле, не типа int).
