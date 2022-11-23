c6e2639964f17d2713f83049042788e0119fa7a9
Costa Varyn <d3tug@yandex.ru>
2022-11-23 11:04:24 +0300

    updated ScanFunc.java

--- src/test/java/stepik/introductory/scanner/basconcepts/ScanFunc.java
+++ src/test/java/stepik/introductory/scanner/basconcepts/ScanFunc.java
@@ -10,11 +10,10 @@ public class ScanFunc {
         double y = 5 * (x*x) + 2*x + 11;
         System.out.println(y);
 
-
-        /**
+        /*
          *         Scanner sc = new Scanner(System.in);
          *         int a = sc.nextInt();
-         *         System.out.print(a + " " + a*a + " " + a*a*a);
+         *         System.out.print(a + " " + a * a + " " + a * a * a);
          */
     }
 }
