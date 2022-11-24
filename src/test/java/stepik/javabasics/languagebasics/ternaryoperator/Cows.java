7c6d3647a48fa73920adbc7b3b33ea9bc4a7affe
Costa Varyn <d3tug@yandex.ru>
2022-11-24 10:36:43 +0300

    updated Cows.java

--- src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Cows.java
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Cows.java
@@ -6,6 +6,7 @@ public class Cows {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.close();
         if (n > 0 && n < 100) {
             if (n % 10 == 1 && n != 11) {
                 System.out.println(n + " корова");
