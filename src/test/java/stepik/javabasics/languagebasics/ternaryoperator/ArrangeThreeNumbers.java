ed0a98751c3ec847b6fe7ea9b2c8245057e9f3a2
Costa Varyn <d3tug@yandex.ru>
2022-11-24 10:36:32 +0300

    updated ArrangeThreeNumbers.java

--- src/test/java/stepik/javabasics/languagebasics/ternaryoperator/ArrangeThreeNumbers.java
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/ArrangeThreeNumbers.java
@@ -6,6 +6,7 @@ public class ArrangeThreeNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        sc.close();
         if (a <= b && b <= c) {
             System.out.println(a + " " + b + " " + c);
         } else if (a <= c && c <= b) {
