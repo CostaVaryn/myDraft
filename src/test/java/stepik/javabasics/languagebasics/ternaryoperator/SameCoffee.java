0153adfb8d22e07104a993ef6ef1d1d3649a0bce
Costa Varyn <d3tug@yandex.ru>
2022-11-24 10:37:46 +0300

    updated SameCoffee.java

--- src/test/java/stepik/javabasics/languagebasics/ternaryoperator/SameCoffee.java
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/SameCoffee.java
@@ -6,6 +6,7 @@ public class SameCoffee {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
+        sc.close();
         System.out.println(age > 20 ? "Да" : "Нет");
     }
 }
