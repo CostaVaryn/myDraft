1112c6b3eae184bd0df9168ca7d8ed7a318917b6
Costa Varyn <d3tug@yandex.ru>
2022-11-24 10:37:04 +0300

    updated Hypotenuse.java

--- src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Hypotenuse.java
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Hypotenuse.java
@@ -6,6 +6,7 @@ public class Hypotenuse {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble(), b = sc.nextDouble();
+        sc.close();
         double c = Math.sqrt(Math.round(Math.pow(a,2) + Math.pow(b,2)));
         System.out.println(c);
     }
