ac151c071f324b3abdb22ee55cb89714a66eeba0
Costa Varyn <d3tug@yandex.ru>
2022-11-24 10:37:20 +0300

    updated PointSix.java

--- src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/PointSix.java
+++ src/test/java/stepik/javabasics/languagebasics/tasksontheconditionaloperatorlevelA/PointSix.java
@@ -6,8 +6,9 @@ public class PointSix {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble(), y = sc.nextDouble();
-        if (((y < ((x*x)-3)) && ((y*y) < (5-(x*x))) && (y > ((0.5*x) - 1))) ||
-                ((y > ((x*x)-3)) && ((y*y) > (5-(x*x))) && (y < ((0.5*x) - 1)))) {
+        sc.close();
+        if (((y < ((x * x) - 3)) && ((y * y) < (5 - (x * x))) && (y > ((0.5 * x) - 1))) ||
+                ((y > ((x * x) - 3)) && ((y * y) > (5 - (x * x))) && (y < ((0.5 * x) - 1)))) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
