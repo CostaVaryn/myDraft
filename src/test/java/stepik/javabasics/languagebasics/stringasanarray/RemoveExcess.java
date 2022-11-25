1f249ab1990d230c8ccba842a835eff39279fdf4
Costa Varyn <d3tug@yandex.ru>
2022-11-25 10:49:19 +0300

    updated RemoveExcess.java

--- src/test/java/stepik/javabasics/languagebasics/stringasanarray/RemoveExcess.java
+++ src/test/java/stepik/javabasics/languagebasics/stringasanarray/RemoveExcess.java
@@ -10,6 +10,7 @@ public class RemoveExcess {
         //System.out.println(str);
 
         char[] a = sc.nextLine().toCharArray();
+        sc.close();
         for (int i = 0; i < a.length; i++) {
             if (Character.isDigit(a[i]) || Character.isLetter(a[i]) || a[i]==' ') {
                 System.out.print(a[i]);
