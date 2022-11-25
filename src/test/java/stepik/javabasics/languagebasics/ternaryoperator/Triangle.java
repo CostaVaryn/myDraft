00aa283343c1d9ef9b1804c471256613732c1487
Costa Varyn <d3tug@yandex.ru>
2022-11-25 10:49:41 +0300

    updated Triangle.java

--- src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Triangle.java
+++ src/test/java/stepik/javabasics/languagebasics/ternaryoperator/Triangle.java
@@ -6,7 +6,8 @@ public class Triangle {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
-        if ((a+b > c) || (a+c > b) || (b+c > a)) {
+        sc.close();
+        if ((a + b > c) || (a + c > b) || (b + c > a)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
