b2e3a32fe9d99efd5eab8053c40387aebf7b8cc1
Costa Varyn <d3tug@yandex.ru>
2022-11-28 16:07:35 +0300

    updated HowManyRoots.java

--- src/test/java/stepik/javabasics/languagebasics/problemsolving/HowManyRoots.java
+++ src/test/java/stepik/javabasics/languagebasics/problemsolving/HowManyRoots.java
@@ -6,8 +6,9 @@ public class HowManyRoots {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
-        int D = (b * b) - (4 * a* c);
-        System.out.println(D == 0 ? "1" : D > 0? "2" : 0);
+        sc.close();
+        int D = (b * b) - (4 * a * c);
+        System.out.println(D == 0 ? "1" : D > 0 ? "2" : 0);
     }
 }
 
