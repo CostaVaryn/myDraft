52e99802830a0ff4eb79e3e5fc5c92becb729ebd
Costa Varyn <d3tug@yandex.ru>
2022-11-30 10:30:25 +0300

    updated LastNum.java

--- src/test/java/stepik/javabasics/languagebasics/basicdatatypes/LastNum.java
+++ src/test/java/stepik/javabasics/languagebasics/basicdatatypes/LastNum.java
@@ -6,6 +6,7 @@ public class LastNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        sc.close();
         System.out.println(a % 10);
     }
 }
