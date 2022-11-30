16334ba4c450f9ad7b15e3972f77c55014248085
Costa Varyn <d3tug@yandex.ru>
2022-11-30 10:30:31 +0300

    updated LeapYear.java

--- src/test/java/stepik/javabasics/languagebasics/conditionaloperator/LeapYear.java
+++ src/test/java/stepik/javabasics/languagebasics/conditionaloperator/LeapYear.java
@@ -6,6 +6,7 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
+        sc.close();
         if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
             System.out.println("Yes");
         } else {
