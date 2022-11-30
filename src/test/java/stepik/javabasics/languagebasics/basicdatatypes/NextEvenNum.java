9339c593e512e79a46a39573cf18f7088077bd4c
Costa Varyn <d3tug@yandex.ru>
2022-11-30 10:30:43 +0300

    updated NextEvenNum.java

--- src/test/java/stepik/javabasics/languagebasics/basicdatatypes/NextEvenNum.java
+++ src/test/java/stepik/javabasics/languagebasics/basicdatatypes/NextEvenNum.java
@@ -6,7 +6,8 @@ public class NextEvenNum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = a-(a%2)+2;
+        sc.close();
+        int b = a - (a % 2) + 2;
         System.out.println(b);
     }
 }
