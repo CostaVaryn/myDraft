0ba667b08980bf79bc1ea1ddcc1d1ca5d2937db8
Costa Varyn <d3tug@yandex.ru>
2022-12-01 12:15:59 +0300

    updated WalleAndEight.java

--- src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndEight.java
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndEight.java
@@ -6,7 +6,8 @@ public class WalleAndEight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), k = sc.nextInt();
-        System.out.println(k/n + " " + k%n);
+        sc.close();
+        System.out.println(k / n + " " + k % n);
     }
 }
 
