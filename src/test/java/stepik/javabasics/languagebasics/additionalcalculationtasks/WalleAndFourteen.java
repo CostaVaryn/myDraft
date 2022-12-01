0a1e04e754ac8c4022aaeb05f59acf50decfd461
Costa Varyn <d3tug@yandex.ru>
2022-12-01 12:16:36 +0300

    updated WalleAndFourteen.java

--- src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndFourteen.java
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndFourteen.java
@@ -6,7 +6,8 @@ public class WalleAndFourteen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt(), y = sc.nextInt(), t = sc.nextInt(), m = sc.nextInt();
-        System.out.println((x*t)+(y*m));
+        sc.close();
+        System.out.println((x * t) + (y * m));
     }
 }
 
