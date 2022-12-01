3fc5b3a8f22baa5808471072cb352d5c2dcb23bb
Costa Varyn <d3tug@yandex.ru>
2022-12-01 12:16:46 +0300

    updated WalleAndOne.java

--- src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndOne.java
+++ src/test/java/stepik/javabasics/languagebasics/additionalcalculationtasks/WalleAndOne.java
@@ -6,8 +6,9 @@ public class WalleAndOne {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        sc.close();
         double d = a + 3;
-        System.out.println(d/2);
+        System.out.println(d / 2);
     }
 }
 
