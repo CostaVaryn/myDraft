15a92efbac357fe2e5d932d765286db0d7c2a2f3
Costa Varyn <d3tug@yandex.ru>
2022-12-02 13:27:40 +0300

    added RanStrGen.java

---
+++ src/test/java/tg/RanStrGen.java
@@ -0,0 +1,15 @@
+package tg;
+
+import java.util.UUID;
+
+public class RanStrGen {
+    public static void main(String[] args) {
+        String randomStr = usingRandomUUID();
+        System.out.println("Случайная строка из 32 символов: " + randomStr);
+        // System.out.println(randomStr.length());
+    }
+    static String usingRandomUUID() {
+        UUID randomUUID = UUID.randomUUID();
+        return randomUUID.toString().replaceAll("_","").replaceAll("-","");
+    }
+}
