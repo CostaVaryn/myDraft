b9e443570de8b87d0bbc40114afea26d5d3a18ca
Costa Varyn <d3tug@yandex.ru>
2022-12-04 16:39:43 +0300

    added GFG1.java

---
+++ src/test/java/tg/GFG1.java
@@ -0,0 +1,26 @@
+package tg;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GFG1 {
+    public static void main(String[] args) throws Exception {
+        try {
+            AbstractList<String> arrlist = new ArrayList<String>();
+
+            arrlist.add("A");
+            arrlist.add("B");
+            arrlist.add("C");
+            arrlist.add("D");
+            arrlist.add("E");
+
+            System.out.println("Original AbstractList: " + arrlist);
+
+            List<String> arrlist2 = arrlist.subList(2, 4);
+            System.out.println("Sublist of AbstractList: " + arrlist2);
+        } catch (IndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
+}
