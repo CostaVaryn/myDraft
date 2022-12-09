df5421a978f6604cb0fd85fd90f75210d337f406
Costa Varyn <d3tug@yandex.ru>
2022-12-09 10:14:50 +0300

    updated TooManyArguments.java

--- src/test/java/stepik/adaptivecourse/TooManyArguments.java
+++ src/test/java/stepik/adaptivecourse/TooManyArguments.java
@@ -8,5 +8,6 @@ public class TooManyArguments {
         String s = sc.nextLine();
         String res = s.replaceAll("[^A-Za-zА-Яа-я.]", "").toLowerCase();
         System.out.println(res);
+        sc.close();
     }
 }
