38eb7622f31c397dba97b44647d6a01c453261cb
Costa Varyn <d3tug@yandex.ru>
2022-12-14 09:46:26 +0300

    add task2.sql

---
+++ src/test/java/stepik/interactiveSQLtrainer/BDandSQLqueries/block5/task2.sql
@@ -0,0 +1,9 @@
+/* Заполнить таблицу step_keyword следующим образом:
+если ключевое слово есть в названии шага, то включить в step_keyword строку с id шага и id ключевого слова. */
+
+INSERT INTO step_keyword (step_id, keyword_id)
+SELECT step_id, keyword_id
+FROM keyword CROSS JOIN step
+WHERE regexp_instr(step_name, concat('\\b', keyword_name, '\\b'));
+
+-- Affected rows: 27
