8975dbd37e432493b6cf51d73587921bfff0f831
Costa Varyn <d3tug@yandex.ru>
2022-12-14 09:46:52 +0300

    add task6.sql

---
+++ src/test/java/stepik/interactiveSQLtrainer/BDandSQLqueries/block4/task6.sql
@@ -0,0 +1,13 @@
+/* Занести в столбец str_id таблицы applicant_order нумерацию абитуриентов,
+которая начинается с 1 для каждой образовательной программы. */
+
+SET @row_num := 1;
+SET @num_pr := 0;
+UPDATE applicant_order
+    SET str_id = IF(program_id = @num_pr, @row_num := @row_num + 1, @row_num := 1 AND @num_pr := @num_pr + 1);
+
+/*
+Affected rows: 0
+Affected rows: 0
+Affected rows: 12
+/*
