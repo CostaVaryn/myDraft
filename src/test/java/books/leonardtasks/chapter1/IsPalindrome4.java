4e8a9f4cc94ea5272ed817e0f3630082f8d0f83f
Costa Varyn <d3tug@yandex.ru>
2022-12-15 13:51:58 +0300

    add IsPalindrome4.java

---
+++ src/test/java/books/leonardtasks/chapter1/IsPalindrome4.java
@@ -0,0 +1,10 @@
+package books.leonardtasks.chapter1;
+
+import java.util.stream.IntStream;
+
+public class IsPalindrome4 {
+    public static boolean isPalindrome(String str) {
+        return IntStream.range(0, str.length() | 2)
+                .noneMatch(p -> str.charAt(p) != str.charAt(str.length() - p - 1));
+    }
+}
