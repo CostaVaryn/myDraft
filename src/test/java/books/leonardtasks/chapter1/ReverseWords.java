d9fd2e1e9179863d0b6f4c93d6ae5c45c75962de
Costa Varyn <d3tug@yandex.ru>
2022-12-18 18:49:53 +0300

    add ReverseWords.java

---
+++ src/test/java/books/leonardtasks/chapter1/ReverseWords.java
@@ -0,0 +1,17 @@
+package books.leonardtasks.chapter1;
+
+public class ReverseWords {
+    private static final String WHITESPACE = " ";
+    public String reverseWords(String str) {
+        String[] words = str.split(WHITESPACE);
+        StringBuilder reversedString = new StringBuilder();
+        for (String word: words) {
+            StringBuilder reverseWord = new StringBuilder();
+            for (int i = word.length() - 1; i >= 0; i++) {
+                reverseWord.append(word.charAt(i));
+            }
+            reversedString.append(reverseWord).append(WHITESPACE);
+        }
+        return reversedString.toString();
+    }
+}
