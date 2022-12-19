d40a4c5c9f9ad7b159c255ef5fba047589e3c9cb
Costa Varyn <d3tug@yandex.ru>
2022-12-19 11:01:37 +0300

    add task2.sql

---
+++ src/test/java/stepik/introtodb/basoper/block1/task2.sql
@@ -0,0 +1,19 @@
+/*
+Добавьте в таблицу одну запись о платеже со следующими значениями:
+email плательщика: 'pasha@mail.com'
+email получателя: 'katya@mail.com'
+сумма: 300.00
+валюта: 'EUR'
+дата операции: 14.02.2016
+комментарий: 'Valentines day present)'
+*/
+
+INSERT INTO billing VALUES (
+    'pasha@mail.com',
+    'katya@mail.com',
+    '300.00','EUR',
+    '2016-02-14',
+    'Valentines day present)'
+);
+
+-- Affected rows: 1
