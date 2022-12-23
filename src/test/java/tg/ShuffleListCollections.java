0ed8674082c3b32cabd586e391429111149a81df
Costa Varyn <d3tug@yandex.ru>
2022-12-23 10:34:35 +0300

    add ShuffleListCollections.java

---
+++ src/test/java/tg/ShuffleListCollections.java
@@ -0,0 +1,15 @@
+package tg;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ShuffleListCollections {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Collections.addAll(list,1, 2, 3, 4, 5);
+        Collections.shuffle(list);
+        for (int i: list) {
+            System.out.println(i);
+        }
+    }
+}
