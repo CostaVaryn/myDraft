7f0569d3c2b5883c48e7e7dfd307d3cb9bee8544
Costa Varyn <d3tug@yandex.ru>
2022-12-26 15:23:54 +0300

    add TestNull.java

---
+++ src/test/java/tg/cd22/dec/TestNull.java
@@ -0,0 +1,13 @@
+package tg.cd22.dec;
+
+public class TestNull {
+    public static void main(String[] args) {
+        method(null);
+    }
+    public static void method(Object o) {
+        System.out.println("Object method");
+    }
+    public static void method(String s) {
+        System.out.println("String method");
+    }
+}
