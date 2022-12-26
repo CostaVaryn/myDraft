8f8a9d748235aa17bef7fffb4e6f346cb4b00451
Costa Varyn <d3tug@yandex.ru>
2022-12-26 15:24:03 +0300

    add TestSumInt.java

---
+++ src/test/java/tg/cd22/dec/TestSumInt.java
@@ -0,0 +1,14 @@
+package tg.cd22.dec;
+
+public class TestSumInt {
+    public static int sum(int x, int ...y) {
+        int s = 0;
+        for (int i = 0; i < y.length; i++) {
+            s += y[i];
+        }
+        return s + x;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(1,2,3,4,5));
+    }
+}
