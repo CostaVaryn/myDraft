7e5db05e6b4d8de106842093ea6cb076badb9181
Costa Varyn <d3tug@yandex.ru>
2022-12-29 16:14:50 +0300

    add ChessClock.java

---
+++ src/test/java/books/leonardtasks/chapter3/ChessClock.java
@@ -0,0 +1,79 @@
+package books.leonardtasks.chapter3;
+
+import java.io.Serializable;
+import java.time.*;
+
+public class ChessClock extends Clock implements Serializable {
+    public enum Player {
+        LEFT,
+        RIGHT
+    }
+    private static final long serialVersionUID = 1L;
+    private Instant instantStart;
+    private Instant instantLeft;
+    private Instant instantRight;
+    private long timeLeft;
+    private long timeRight;
+    private Player player;
+
+    public ChessClock(Player player) {
+        this.player = player;
+    }
+
+    public Instant gameStart() {
+        if (this.instantStart == null) {
+            this.timeLeft = 0;
+            this.timeRight = 0;
+            this.instantStart = Instant.now();
+            this.instantLeft = instantStart;
+            this.instantRight = instantStart;
+            return instantStart;
+        }
+        throw new IllegalStateException(
+                "Игра уже началась. Остановите ее и попробуйте еще раз.");
+    }
+    public Instant gameEnd() {
+        if (this.instantStart != null) {
+            instantStart = null;
+            return Instant.now();
+        }
+        throw new IllegalStateException("Игра еще не началась.");
+    }
+    @Override
+    public ZoneId getZone() {
+        return ZoneOffset.UTC;
+    }
+    @Override
+    public Clock withZone(ZoneId zone) {
+        throw new UnsupportedOperationException(
+                "Класс ChessClock работает только в часовом поясе UTC");
+    }
+    @Override
+    public Instant instant() {
+        if (this.instantStart != null) {
+            if (player == Player.LEFT) {
+                player = Player.RIGHT;
+                long secondsLeft = Instant.now().getEpochSecond()
+                        - instantRight.getEpochSecond();
+                instantLeft = instantLeft.plusSeconds(secondsLeft - timeLeft);
+                timeLeft = secondsLeft;
+                return instantLeft;
+            } else {
+                player = Player.LEFT;
+                long secondsRight = Instant.now().getEpochSecond()
+                        - instantLeft.getEpochSecond();
+                instantRight = instantRight.plusSeconds(secondsRight - timeRight);
+                timeRight = secondsRight;
+                return instantRight;
+            }
+        }
+        throw new IllegalStateException("Игра еще не началась.");
+    }
+
+    public static void main(String[] args) {
+        ChessClock chessClock = new ChessClock(Player.LEFT);
+        Instant start = chessClock.gameStart();
+        Instant end = chessClock.gameEnd();
+        Duration.between(start, end).getSeconds();
+    }
+}
