c37e6f43ab3b36d77d21d9374843bc18ebf12dcd
Costa Varyn <d3tug@yandex.ru>
2023-01-10 16:27:29 +0300

    add AbstractPhone.java

---
+++ src/test/java/jr/AbstractPhone.java
@@ -0,0 +1,11 @@
+package jr;
+
+public abstract class AbstractPhone {
+    private int year;
+
+    public AbstractPhone(int year) {
+        this.year = year;
+    }
+    public abstract void call(int outputNumber);
+    public abstract void ring (int inputNumber);
+}
