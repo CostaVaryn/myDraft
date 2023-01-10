da4336478de3b2c7f4102c9dd2923f32c25c5bff
Costa Varyn <d3tug@yandex.ru>
2023-01-10 16:27:38 +0300

    add MainPhone.java

---
+++ src/test/java/jr/MainPhone.java
@@ -0,0 +1,15 @@
+package jr;
+
+public class MainPhone {
+    public static void main(String[] args) {
+        AbstractPhone firstPhone = new ThomasEdisonPhone(1878);
+        AbstractPhone phone = new Phone(1927);
+        AbstractPhone videoPhone=new VideoPhone(1961);
+        User user = new User("Mark");
+        user.callAnotherUser(2_25_17,firstPhone);
+
+        user.callAnotherUser(2_25_17,phone);
+
+        user.callAnotherUser(2_25_17,videoPhone);
+    }
+}
