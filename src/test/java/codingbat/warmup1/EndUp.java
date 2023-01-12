ab1d0a9f17e16a274b770164be95f66815689ac2
Costa Varyn <d3tug@yandex.ru>
2023-01-12 13:29:05 +0300

    add EndUp.java

---
+++ src/test/java/codingbat/warmup1/EndUp.java
@@ -0,0 +1,18 @@
+package codingbat.warmup1;
+
+public class EndUp {
+    public static String endUp(String str) {
+        if (str.length() <= 3) return str.toUpperCase();
+        int cut = str.length() - 3;
+        String front = str.substring(0, cut);
+        String back  = str.substring(cut);
+
+        return front + back.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(endUp("Hello"));
+        System.out.println(endUp("hi there"));
+        System.out.println(endUp("hi"));
+    }
+}
