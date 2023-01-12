9d52a03ff2ff3352b61913e0c58a6df9afd420ca
Costa Varyn <d3tug@yandex.ru>
2023-01-12 13:28:53 +0300

    add Close10.java

---
+++ src/test/java/codingbat/warmup1/Close10.java
@@ -0,0 +1,22 @@
+package codingbat.warmup1;
+
+public class Close10 {
+    public static int close10(int a, int b) {
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (aDiff > bDiff) {
+            return b;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(close10(8, 13));
+        System.out.println(close10(13, 8));
+        System.out.println(close10(13, 7));
+    }
+}
