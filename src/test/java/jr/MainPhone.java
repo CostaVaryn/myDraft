8d026efed15f6123cd043c12bf31dc49e3fd0e0f
Costa Varyn <d3tug@yandex.ru>
2023-01-13 10:44:22 +0300

    update MainPhone.java

--- src/test/java/jr/MainPhone.java
+++ src/test/java/jr/MainPhone.java
@@ -4,12 +4,12 @@ public class MainPhone {
     public static void main(String[] args) {
         AbstractPhone firstPhone = new ThomasEdisonPhone(1878);
         AbstractPhone phone = new Phone(1927);
-        AbstractPhone videoPhone=new VideoPhone(1961);
+        AbstractPhone videoPhone = new VideoPhone(1961);
         User user = new User("Mark");
-        user.callAnotherUser(2_25_17,firstPhone);
+        user.callAnotherUser(2_25_17, firstPhone);
 
-        user.callAnotherUser(2_25_17,phone);
+        user.callAnotherUser(2_25_17, phone);
 
-        user.callAnotherUser(2_25_17,videoPhone);
+        user.callAnotherUser(2_25_17, videoPhone);
     }
 }
