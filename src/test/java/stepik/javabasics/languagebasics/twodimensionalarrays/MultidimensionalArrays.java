7b35f45d8a8205b04865bedf4b49cbe17dae8454
Costa Varyn <d3tug@yandex.ru>
2023-01-13 10:44:48 +0300

    update MultidimensionalArrays.java

--- src/test/java/stepik/javabasics/languagebasics/twodimensionalarrays/MultidimensionalArrays.java
+++ src/test/java/stepik/javabasics/languagebasics/twodimensionalarrays/MultidimensionalArrays.java
@@ -10,14 +10,14 @@ public class MultidimensionalArrays {
         int[][][] mas = new int[n][m][p];
 
         // пример заполнения
-        for (int i = 0; i<n; i++)
-            for (int j = 0; j<m; j++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
                 for (int k = 0; k < p; k++)
                     mas[i][j][k] = 0;
 
         //пример вывода
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print("(");
                 for (int k = 0; k < p; k++)
                     System.out.print(mas[i][j][k] + " ");
