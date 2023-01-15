3cbfd5b8f61720d725e07a9c51220eae1b40e115
Costa Varyn <d3tug@yandex.ru>
2023-01-15 11:46:33 +0300

    add SomePhone.java

---
+++ src/test/java/jr/SomePhone.java
@@ -0,0 +1,23 @@
+package jr;
+
+public class SomePhone {
+
+    private int year;
+    private String company;
+    public SomePhone(int year, String company) {
+        this.year = year;
+        this.company = company;
+    }
+    private void openConnection(){
+        //findComutator
+        //openNewConnection...
+    }
+    public void call() {
+        openConnection();
+        System.out.println("Call number");
+    }
+
+    public void ring() {
+        System.out.println("Ring ring");
+    }
+}
