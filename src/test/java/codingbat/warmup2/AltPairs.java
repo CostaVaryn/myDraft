856ef130584f13ec7765dd7e19f444fdfa1aca3b
Costa Varyn <d3tug@yandex.ru>
2023-01-17 09:57:42 +0300

    add AltPairs.java

---
+++ src/test/java/codingbat/warmup2/AltPairs.java
@@ -0,0 +1,15 @@
+package codingbat.warmup2;
+
+public class AltPairs {
+    public String altPairs(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i += 4) {
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result = result + str.substring(i, end);
+        }
+        return result;
+    }
+}
