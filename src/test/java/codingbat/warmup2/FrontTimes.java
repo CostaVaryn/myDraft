728949104306768cb50fa0a279fe67e7775e12a2
Costa Varyn <d3tug@yandex.ru>
2023-01-19 12:50:40 +0300

    add FrontTimes.java

---
+++ src/test/java/codingbat/warmup2/FrontTimes.java
@@ -0,0 +1,23 @@
+package codingbat.warmup2;
+
+public class FrontTimes {
+    public static String frontTimes(String str, int n) {
+        int frontLen = 3;
+        if (frontLen > str.length()) {
+            frontLen = str.length();
+        }
+        String front = str.substring(0, frontLen);
+
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += front;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(frontTimes("Chocolate", 2));
+        System.out.println(frontTimes("Chocolate", 3));
+        System.out.println(frontTimes("Abc", 3));
+    }
+}
