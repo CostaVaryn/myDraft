f797fba94326724e6c9a7ab2e72a7280d21432a5
Costa Varyn <d3tug@yandex.ru>
2023-01-24 10:42:14 +0300

    add ComboString.java

---
+++ src/test/java/codingbat/string1/ComboString.java
@@ -0,0 +1,18 @@
+package codingbat.string1;
+
+public class ComboString {
+    public static String comboString(String a, String b) {
+        if (a.length() > b.length()) {
+            return b + a + b;
+        } else {
+            return a + b + a;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(comboString("Hello", "hi"));
+        System.out.println(comboString("hi", "Hello"));
+        System.out.println(comboString("aaa", "b"));
+    }
+}
