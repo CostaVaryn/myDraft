ef26e6faacb6ec15949341cbcddb781ebcc3f2ab
Costa Varyn <d3tug@yandex.ru>
2023-01-28 11:06:28 +0300

    add StringX.java

---
+++ src/test/java/codingbat/warmup2/StringX.java
@@ -0,0 +1,13 @@
+package codingbat.warmup2;
+
+public class StringX {
+    public String stringX(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (!(i > 0 && i < (str.length() - 1) && str.charAt(i) == 'x')) {
+                result = result + str.charAt(i);
+            }
+        }
+        return result;
+    }
+}
