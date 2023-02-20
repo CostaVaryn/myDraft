21532df9c69a62cf796732a4cad257a2c3562623
Costa Varyn <d3tug@yandex.ru>
2023-02-20 17:45:06 +0300

    add BaseTest.java

---
+++ src/test/java/org/qa/uiTest/BaseTest.java
@@ -0,0 +1,18 @@
+package org.qa.uiTest;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+    @BeforeAll
+    public static void setUp() {
+        Configuration.headless = false;
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        Selenide.closeWebDriver();
+    }
+}
