949b8c74b9db503f76b8c5f23e85949a09528a7a
Costa Varyn <d3tug@yandex.ru>
2023-03-20 09:28:44 +0300

    add ProgShape.java

---
+++ src/test/java/writer/ProgShape.java
@@ -0,0 +1,24 @@
+package writer;
+
+abstract class Shape {
+    int width;
+    abstract void area();
+}
+
+class Square extends Shape {
+    Square (int x) {
+        width = x;
+    }
+    @Override
+    void area() {
+        System.out.println(width*width);
+    }
+}
+
+public class ProgShape {
+    public static void main(String[] args) {
+        int x = 5;
+        Square a = new Square(x);
+        a.area();
+    }
+}
