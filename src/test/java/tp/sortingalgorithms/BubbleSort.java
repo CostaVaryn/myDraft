6baec5804ffcc0737c14867124b9c16994c63547
Constantin Varyn <d3tug@yandex.ru>
2023-04-09 08:03:14 +0300

    refactor

--- src/test/java/tp/sortingalgorithms/BubbleSort.java
+++ src/test/java/tp/sortingalgorithms/BubbleSort.java
@@ -1,10 +1,10 @@
 package tp.sortingalgorithms;
 
 public class BubbleSort {
-    public static void bubbleSort(int[] sortArr){
+    public static void bubbleSort(int[] sortArr) {
         for (int i = 0; i < sortArr.length - 1; i++) {
-            for(int j = 0; j < sortArr.length - i - 1; j++) {
-                if(sortArr[j + 1] < sortArr[j]) {
+            for (int j = 0; j < sortArr.length - i - 1; j++) {
+                if (sortArr[j + 1] < sortArr[j]) {
                     int swap = sortArr[j];
                     sortArr[j] = sortArr[j + 1];
                     sortArr[j + 1] = swap;
