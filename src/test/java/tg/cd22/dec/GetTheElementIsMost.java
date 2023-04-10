ce423dfebd0bb0ebdf5a19408fc766a95d8b37aa
Constantin Varyn <d3tug@yandex.ru>
2023-04-10 09:07:58 +0300

    refactor

--- src/test/java/tg/cd22/dec/GetTheElementIsMost.java
+++ src/test/java/tg/cd22/dec/GetTheElementIsMost.java
@@ -7,7 +7,8 @@ import java.util.stream.Stream;
 
 public class GetTheElementIsMost {
     public static void main(String[] args) {
-        Stream.of(1,2,12,4,5,6,5,4,3,4,5,4,3,2,3,4,3,2,4,2,3,2,4,4,5,6,3,3,2,2,3,3,4,4,4,4,5,5,5,5,5,5)
+        Stream.of(1, 2, 12, 4, 5, 6, 5, 4, 3, 4, 5, 4, 3, 2, 3, 4, 3, 2, 4, 2,
+                        3, 2, 4, 4, 5, 6, 3, 3, 2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
