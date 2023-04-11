8ba402ee8ccb86a7811dad227896c1abd36929cc
Constantin Varyn <d3tug@yandex.ru>
2023-04-11 09:40:55 +0300

    refactor

--- src/test/java/tg/cd22/dec/ShuffleListCollections.java
+++ src/test/java/tg/cd22/dec/ShuffleListCollections.java
@@ -6,9 +6,9 @@ import java.util.Collections;
 public class ShuffleListCollections {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        Collections.addAll(list,1, 2, 3, 4, 5);
+        Collections.addAll(list, 1, 2, 3, 4, 5);
         Collections.shuffle(list);
-        for (int i: list) {
+        for (int i : list) {
             System.out.println(i);
         }
     }
