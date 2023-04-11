a4904123962de221aecb673e167c96c2d5849f5a
Constantin Varyn <d3tug@yandex.ru>
2023-04-11 09:41:03 +0300

    refactor

--- src/test/java/tg/cd22/dec/RemoveIf.java
+++ src/test/java/tg/cd22/dec/RemoveIf.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class RemoveIf {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
         numbers.removeIf(s -> s > 5);
         numbers.forEach(s -> System.out.println(s + " "));
     }
