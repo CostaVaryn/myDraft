29cf8e98fe0d7b8ac7157ce7b27bbfd7a2cbd68b
Constantin Varyn <d3tug@yandex.ru>
2023-04-13 13:45:11 +0300

    add MouseCaptureDemo.java

---
+++ src/test/java/tg/cd23/jan/MouseCaptureDemo.java
@@ -0,0 +1,30 @@
+package tg.cd23.jan;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseCaptureDemo extends JFrame implements MouseMotionListener {
+    public JLabel mouseHoverStatus;
+
+    public static void main(String[] args) {
+        new MouseCaptureDemo();
+    }
+
+    MouseCaptureDemo() {
+        setSize(500, 500);
+        setTitle("Frame displaying Coordinates of Mouse Motion");
+
+        mouseHoverStatus = new JLabel("No Mouse Hover Detected.", JLabel.CENTER);
+        add(mouseHoverStatus);
+        addMouseMotionListener(this);
+        setVisible(true);
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        mouseHoverStatus.setText("Mouse Cursor Coordinates => X:" + e.getX() + " | Y:" + e.getY());
+    }
+
+    public void mouseDragged(MouseEvent e) {
+    }
+}
